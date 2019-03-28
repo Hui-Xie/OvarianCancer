bc77f8c0f6eaa6737f36c1848a041644301b9f21
Hui Xie <sheenxh@gmail.com>
2019-03-28 14:27:08 -0500

    support crop volume

--- DataMgr.py
+++ DataMgr.py
@@ -1,5 +1,7 @@
 import os
 import SimpleITK as sitk
+import matplotlib.pyplot as plt
+
 
 class DataMgr:
     def __init__(self, imagesDir, labelsDir):
@@ -20,3 +22,30 @@ class DataMgr:
 
     def getLabelFile(self, imageFile):
         return imageFile.replace("_CT.nrrd", "_Seg.nrrd").replace("/images/", "/labels/")
+
+    def getLabeledSliceIndex(self, labelArray):
+        nonzeroSlices = labelArray.sum((1,2)).nonzero(); # a tuple of arrays
+        nonzeroSlices = nonzeroSlices[0]
+        print(nonzeroSlices)
+        result = []
+        previous = nonzeroSlices[0]
+        start = previous
+        for index in nonzeroSlices:
+            if index- previous <= 1:
+                previous = index
+                continue
+            else:
+                result.append((previous+start)/2)
+                start=previous=index
+        result.append((previous + start) / 2)
+        return [int(round(x,0)) for x in result]
+
+    def displaySlices(self, array, sliceList):
+        N = len(sliceList)
+        for i in range(N):
+            plt.subplot(1,N, i+1)
+            plt.imshow(array[sliceList[i],:,:])
+        plt.show()
+
+    def cropVolume(self,array, center, radius):
+        return array[center-redius: center+radius+1,:,:].copy()
--- TrainSegV.py
+++ TrainSegV.py
@@ -9,10 +9,24 @@ def main():
 
     dataMgr = DataMgr(sys.argv[1], sys.argv[2])
     filesList = dataMgr.getFilesList(dataMgr.m_imagesDir, "_CT.nrrd")
-    print(filesList)
-    print(len(filesList))
-    print(dataMgr.getLabelFile(filesList[0]))
-    dataMgr.readImageFile(filesList[0])
+    #imageFile = filesList[0]
+    imageFile = "/home/hxie1/data/OvarianCancerCT/Extract_uniform/images/01626917_CT.nrrd"
+
+
+
+    labelFile = dataMgr.getLabelFile(imageFile)
+    print("labelFile: ", labelFile)
+    labelArray = dataMgr.readImageFile(labelFile)
+    sliceIndex = dataMgr.getLabeledSliceIndex(labelArray)
+    print(sliceIndex)
+
+    print(imageFile)
+    imageArray = dataMgr.readImageFile(imageFile)
+    dataMgr.displaySlices(imageArray.clip(-400, 500), sliceIndex)
+    dataMgr.displaySlices(labelArray, sliceIndex)
+
+
+
 
 if __name__ == "__main__":
     main()
