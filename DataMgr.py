759becedf79083abed5136e353142db1cf61520c
Hui Xie <sheenxh@gmail.com>
2019-03-28 11:03:51 -0500

    add simpleITK reading file

--- DataMgr.py
+++ DataMgr.py
@@ -1,4 +1,5 @@
 import os
+import SimpleITK as sitk
 
 class DataMgr:
     def __init__(self, imagesDir, labelsDir):
@@ -6,5 +7,16 @@ class DataMgr:
         self.m_labelsDir = labelsDir
 
     def getFilesList(self, filesDir, suffix):
-        return [os.path.abspath(x) for x in os.listdir(filesDir) if suffix in x]
+        originalCwd = os.getcwd()
+        os.chdir(filesDir)
+        filesList = [os.path.abspath(x) for x in os.listdir(filesDir) if suffix in x]
+        os.chdir(originalCwd)
+        return filesList
 
+    def readImageFile(self, filename):
+        image = sitk.ReadImage(filename)
+        dataArray = sitk.GetArrayFromImage(image) #numpy axis order is a reverse of ITK axis order
+        return dataArray
+
+    def getLabelFile(self, imageFile):
+        return imageFile.replace("_CT.nrrd", "_Seg.nrrd").replace("/images/", "/labels/")
--- TrainSegV.py
+++ TrainSegV.py
@@ -6,11 +6,13 @@ def main():
     if len(sys.argv) != 3:
         print("Error: input parameters error.")
         return -1
-    else:
-        dataMgr = DataMgr(sys.argv[1], sys.argv[2])
-        filesList = dataMgr.getFilesList(dataMgr.m_imagesDir, "_CT.nrrd")
-        print(filesList)
-        print(len(filesList))
+
+    dataMgr = DataMgr(sys.argv[1], sys.argv[2])
+    filesList = dataMgr.getFilesList(dataMgr.m_imagesDir, "_CT.nrrd")
+    print(filesList)
+    print(len(filesList))
+    print(dataMgr.getLabelFile(filesList[0]))
+    dataMgr.readImageFile(filesList[0])
 
 if __name__ == "__main__":
     main()
