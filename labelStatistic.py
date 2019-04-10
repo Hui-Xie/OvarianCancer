d9890dac530516c6fc99424803f012b00c019351
Hui Xie <sheenxh@gmail.com>
2019-04-10 17:00:46 -0500

    add weight computation of label distribution

--- labelStatistic.py
+++ labelStatistic.py
@@ -9,17 +9,25 @@ trainDataMgr.setDataSize(64, 21,281,281,4)  #batchSize, depth, height, width, k
 trainDataMgr.setMaxShift(25)                #translation data augmentation
 trainDataMgr.setFlipProb(0.3)               #flip data augmentation
 
-labelStatis = [0, 0, 0, 0]  # the number of pixels labeled as 0, 1,2,3
+pixelStatis = [0, 0, 0, 0]  # the number of pixels labeled as 0, 1,2,3
 sliceStatis = [0, 0, 0, 0]  # the number of slices having label 0, 1, 2, 3
 
 print("Start to statistics the label data, please waiting......")
 
 for _, labelsCpu in trainDataMgr.dataLabelGenerator(False):
-    (labelList, sliceList) = trainDataMgr.batchLabelStatistic(labelsCpu, 4)
-    labelStatis = [x+y for x, y in zip(labelStatis,labelList)]
+    (pixelList, sliceList) = trainDataMgr.batchLabelStatistic(labelsCpu, 4)
+    pixelStatis = [x + y for x, y in zip(pixelStatis, pixelList)]
     sliceStatis = [x+y for x, y in zip(sliceStatis,sliceList)]
 
-print("Label statistics: ", labelStatis)
+print("in below statistic list,  positions indicate label 0, 1, 2 ,3")
+
+print("pixels statistics: ", pixelStatis)
+sumPixels = sum(pixelStatis)
+pixelProb = [x/sumPixels for x in pixelStatis]
+print("pixels probability: ", pixelProb)
+weights = [1/x for x in pixelProb]
+print("Label weights in Cross Entropy: ", weights)
+
 print("Slice statistics: ", sliceStatis)
-
-
+sumSlices = sliceStatis[0]
+print("slice probability: ", [x/sumSlices for x in sliceStatis])
