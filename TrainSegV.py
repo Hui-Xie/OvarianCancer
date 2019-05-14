5171107ab1a4a56e7e9ccb99242095c638d09a4e
Hui Xie <sheenxh@gmail.com>
2019-05-14 13:12:27 -0500

    debug

--- TrainSegV.py
+++ TrainSegV.py
@@ -82,8 +82,8 @@ def main():
     testDataMgr.setRemainedLabel(3, labelTuple)
 
     # ===========debug==================
-    trainDataMgr.setOneSampleTraining(True)  # for debug
-    testDataMgr.setOneSampleTraining(True)  # for debug
+    trainDataMgr.setOneSampleTraining(False)  # for debug
+    testDataMgr.setOneSampleTraining(False)  # for debug
     useDataParallel = True  # for debug
     # ===========debug==================
 
