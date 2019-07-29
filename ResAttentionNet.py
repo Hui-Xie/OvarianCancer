15a2f928d9f80d0cacac4d3267c7c2e8a3598da4
Hui Xie <sheenxh@gmail.com>
2019-07-29 09:28:30 -0500

    start to further reduce parameters

--- ResAttentionNet.py
+++ ResAttentionNet.py
@@ -9,7 +9,7 @@ class ResAttentionNet(BasicModel):
     def __init__(self):
         super().__init__()
         # For input image size: 140*251*251 (zyx)
-        # at July 28 02:20, 2019, reduce network parameters again from  1.23 million parameters to
+        # at July 29 09:25, 2019, continue to reduce network parameters again from  1.23 million parameters to ****
         # use average pooling. log_ResAttention_CV0_20190727_1708.txt
         # todo 
         self.m_stage0 = nn.Sequential(
