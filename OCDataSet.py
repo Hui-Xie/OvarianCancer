b168439f1db813163ea3202c0a77ff97d8884554
Hui Xie <sheenxh@gmail.com>
2019-08-09 17:02:13 -0500

    rotate test fold for K-fold CV

--- OCDataSet.py
+++ OCDataSet.py
@@ -11,13 +11,13 @@ import torch
 from torch.utils import data
 
 class OVDataPartition():
-    def __init__(self, inputsDir, labelsPath, inputSuffix, K_fold, testProportion, logInfoFun=print):
+    def __init__(self, inputsDir, labelsPath, inputSuffix, K_fold, k, logInfoFun=print):
         self.m_inputsDir = inputsDir
         self.m_inputFilesListFile = os.path.join(self.m_inputsDir, "inputFilesList.txt")
         self.m_labelsPath = labelsPath
         self.m_inputSuffix = inputSuffix
         self.m_KFold = K_fold
-        self.m_testProportion = testProportion
+        self.m_k = k
         self.m_logInfo = logInfoFun
 
         self.updateInputsList()
@@ -62,51 +62,35 @@ class OVDataPartition():
         N = len(self.m_labelsList)
         N0 = len(self.m_0FileIndices)
         N1 = len(self.m_1FileIndices)
-        Ntest = int(math.ceil(N*self.m_testProportion))
 
-        random.seed(201907)
+        random.seed(201908)
         random.shuffle(self.m_0FileIndices)
         random.shuffle(self.m_1FileIndices)
 
-        nTest0 = int(N0 * self.m_testProportion)
-        nTest1 = Ntest- nTest0
+        folds0 = np.array_split(np.asarray(self.m_0FileIndices), self.m_KFold)
+        folds1 = np.array_split(np.asarray(self.m_1FileIndices), self.m_KFold)
 
         self.m_partition = {}
-        self.m_partition["test"]  = self.m_0FileIndices[0:nTest0] + self.m_1FileIndices[0:nTest1]
-        self.m_partition["train0s"] =  np.array_split(np.asarray(self.m_0FileIndices[nTest0:]), self.m_KFold)
-        self.m_partition["train1s"] =  np.array_split(np.asarray(self.m_1FileIndices[nTest1:]), self.m_KFold)
+        k = self.m_k
+        K = self.m_KFold
+        self.m_partition["test"] = folds0[k].tolist()+ folds1[k].tolist()
+        k1 = (k+1)% K  # validation k
+        self.m_partition["validation"] =  folds0[k1].tolist()+ folds1[k1].tolist()
+        self.m_partition["training"] = []
+        for i in range(K):
+            if i != k and i != k1:
+                self.m_partition["training"] += folds0[i].tolist()+ folds1[i].tolist()
+        self.m_logInfo(f"{K}-fold cross validation: the {k}th fold for test, the {k1}th fold for validation, remaining folds for training.")
 
-        self.m_logInfo(f"Infor: the independent test set has {Ntest} files,and Training including validation Set has {N-Ntest} files which will be divided into {self.m_KFold} folds.")
-        self.m_logInfo(f"In the independent test set of {Ntest} files, 0 has {nTest0}, 1 has {nTest1}, the rate of 1s is {nTest1/Ntest}")
 
-    def getLabels(self, dataIDs):
-        labels =[]
-        for id in dataIDs:
-            label = self.m_labelsList[id]
-            labels.append(label)
-        return labels
 
 class OVDataSet(data.Dataset):
-    def __init__(self, dataPartitions, partitionName, k, transform=None, logInfoFun=print):
-        self.m_dataPartioins = dataPartitions
+    def __init__(self, partitionName, partition, transform=None, logInfoFun=print):
+        self.m_dataIDs = partition
         self.m_transform = transform
         self.m_logInfo = logInfoFun
-        K = self.m_dataPartioins.m_KFold
-        self.m_dataIDs = []
-        if "test" == partitionName:
-            self.m_dataIDs = self.m_dataPartioins.m_partition["test"]
-        elif "train" == partitionName and k < K:
-            for i in range(K):
-                if i != k:
-                    self.m_dataIDs += self.m_dataPartioins.m_partition["train0s"][i].tolist() + self.m_dataPartioins.m_partition["train1s"][i].tolist()
-        elif "validation" == partitionName and k < K:
-            self.m_dataIDs += self.m_dataPartioins.m_partition["train0s"][k].tolist() + self.m_dataPartioins.m_partition["train1s"][k].tolist()
-        else:
-            self.m_logInfo("Error: partitionName or k in OvDataSet are incorrect")
-            sys.exit(0)
-
-        self.m_labels = self.m_dataPartioins.getLabels(self.m_dataIDs)
-        self.m_logInfo(f"In this {partitionName}_{k} dataset, total {len(self.m_labels)} files, where 1 has {sum(self.m_labels)} with rate of {sum(self.m_labels)/len(self.m_labels)}")
+        self.m_labels = self.getLabels(self.m_dataIDs)
+        self.m_logInfo(f"In {partitionName} dataset, total {len(self.m_labels)} files, where 1 has {sum(self.m_labels)} with rate of {sum(self.m_labels)/len(self.m_labels)}")
 
     def __len__(self):
         return len(self.m_labels)
@@ -121,3 +105,10 @@ class OVDataSet(data.Dataset):
             data = self.m_transform(data)
 
         return data, label
+
+    def getLabels(self, dataIDs):
+        labels = []
+        for id in dataIDs:
+            label = self.m_labelsList[id]
+            labels.append(label)
+        return labels
