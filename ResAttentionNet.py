fix an important bug: SITK read nrrd into int type, we must convert it into float type before normalization

ResAttentionNet.py
@@ -8,6 +8,8 @@ import torch
 
 class ResAttentionNet(BasicModel):
     def forward(self, x):
+
+        x = self.stn(x)
         x = self.m_stage0(x)
         x = self.m_stage1(x)
         x = self.m_stage2(x)
@@ -27,14 +29,13 @@ class ResAttentionNet(BasicModel):
         # add maxPool at each stage, and 1024 is the final conv filter number.
         # log:
         #
-        # result: 
+        # result:
         #
+        self.m_stn    = SpatialTransformer(231,32, 251,251)
         self.m_stage0 = nn.Sequential(
-                        SpatialTransformer(231,32, 251,251),
                         ResNeXtBlock(231, 32, nGroups=33, poolingLayer=None),
                         ResNeXtBlock(32, 32, nGroups=8, poolingLayer=None),
                         ResNeXtBlock(32, 64, nGroups=8, poolingLayer=None)
-
                         )  # ouput size: 64*251*251
         self.m_stage1 = nn.Sequential(
                         ResNeXtBlock(64, 64, nGroups=16, poolingLayer=nn.MaxPool2d(3,stride=2, padding=1)),