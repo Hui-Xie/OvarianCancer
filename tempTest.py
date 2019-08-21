1e20807b0fbf669ef2a65ddf700d0136ad71603d
Hui Xie <sheenxh@gmail.com>
2019-08-21 11:01:31 -0500

    update log

--- tempTest.py
+++ tempTest.py
@@ -1,41 +1,27 @@
 
-from DataMgr import DataMgr
-import numpy as np
+import torch
+
+A = torch.tensor([[1,2,3],[3,4,5]],requires_grad= True, dtype=torch.float)
+
+b = torch.tensor([0.5,2,1])
+
+print("Before backward: A.grad", A.grad)
+
+B = A.clone()
+C = B.clone()
+
+y = torch.mean(C*b)
+y.backward()
+
+print("After backward, A.grad", A.grad)
 
 
-array = np.array([[1,2,3],[2,2,2],[7,5,9]], float)
+#y = torch.mean(A*b)
+#y.backward()
 
-axesTuple = tuple([x for x in range(1, array.ndim)])
-minx = np.min(array, axesTuple)
-result = np.zeros(array.shape)
-for i in range(len(minx)):
-    result[i, :] = array[i, :] - minx[i]
-ptp = np.ptp(array, axesTuple)  # peak to peak
-with np.nditer(ptp, op_flags=['readwrite']) as it:
-    for x in it:
-        if 0 == x:
-            x[...] = 1e-6
-for i in range(len(ptp)):
-    result[i, :] /= ptp[i]
+# print(A.grad)
+# tensor([[0.0833, 0.3333, 0.1667],
+#         [0.0833, 0.3333, 0.1667]])
 
-
-print(result)
-
-
-class A():
-    def __init__(self):
-        pass
-
-    def __call__(self,x):
-        print(f"x= {x}")
-
-a = A();
-
-if a:
-   a(5)
-
-a = None
-if a:
-    a(10)
 
 
