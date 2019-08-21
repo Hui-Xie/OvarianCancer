8d1a74e84f0044370adcb01327808a3089783db1
Sheen <sheenxh@gmail.com>
2019-08-20 19:39:10 -0500

    debug

--- ResAttentionNet.py
+++ ResAttentionNet.py
@@ -20,18 +20,17 @@ class ResAttentionNet(BasicModel):
         x = self.m_stage2(x)
         x = self.m_stage3(x)
 
-        with torch.autograd.set_detect_anomaly(True):
-            x = self.m_stage4(x)
-            stn4,m4 = self.m_stn4(x) # m is modulation factor
-            B = x.shape[0]
-            for b in range(B):
-                x[b,] = x[b,].clone() + m4[b]*stn4[b,]     # x = x+ m*stn
+        x = self.m_stage4(x)
+        stn4,m4 = self.m_stn4(x) # m is modulation factor
+        B = x.shape[0]
+        for b in range(B):
+            x[b,] = x[b,] + m4[b]*stn4[b,]     # x = x+ m*stn
 
-            x = self.m_stage5(x)
-            stn5, m5 = self.m_stn5(x)  # m is modulation factor
-            B = x.shape[0]
-            for b in range(B):
-                x[b,] = x[b,].clone() + m5[b] * stn5[b,]   # x = x+ m*stn
+        x = self.m_stage5(x)
+        stn5, m5 = self.m_stn5(x)  # m is modulation factor
+        B = x.shape[0]
+        for b in range(B):
+            x[b,] = x[b,] + m5[b] * stn5[b,]   # x = x+ m*stn
 
         x = self.m_layersBeforeFc(x)
         x = torch.reshape(x, (x.shape[0], x.numel() // x.shape[0]))
--- SpatialTransformer.py
+++ SpatialTransformer.py
@@ -44,16 +44,18 @@ class SpatialTransformer(nn.Module):
             xs = layer(xs)
         xs = torch.reshape(xs, (xs.shape[0], xs.numel() // xs.shape[0]))
         xs = self.m_regression(xs)
-        theta = xs[:,0:6].clone()
-        m = xs[:,6].clone()  # modulation factor
-        m = torch.sigmoid(m)     # convert into range [0,1]
 
-        theta = theta.view(-1, 2, 3)
+        with torch.autograd.set_detect_anomaly(True):
+            theta = xs[:,0:6].clone()
+            m = xs[:,6].clone()  # modulation factor
+            m = torch.sigmoid(m)     # convert into range [0,1]
 
-        theta = self.spectralNormalize(theta)  # Spectral Normalize to reduce image repeating.
+            theta = theta.view(-1, 2, 3)
 
-        grid = F.affine_grid(theta, x.size())
-        xout = F.grid_sample(x, grid, padding_mode="reflection")
+            theta = self.spectralNormalize(theta)  # Spectral Normalize to reduce image repeating.
+
+            grid = F.affine_grid(theta, x.size())
+            xout = F.grid_sample(x, grid, padding_mode="reflection")
         return xout, m
 
     def spectralNormalize(self, theta):
