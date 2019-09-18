abcd4b40bdbea42117d81849fe7b7f9e474a6cb1
Hui Xie <sheenxh@gmail.com>
2019-09-18 14:53:40 -0500

    refine code

---
+++ Tools/generateLatexReport2.py
@@ -0,0 +1,123 @@
+
+import os
+
+latexHead = r'''
+
+\documentclass[12pt]{article}
+\usepackage[margin=0.8in]{geometry}
+\usepackage{graphicx}
+\usepackage{subcaption}
+\usepackage[T1]{fontenc}  %do not interpret underscore
+
+\geometry{a4paper}
+
+\title{Ovarian Cancer Segmentation Test Report}
+\author{Hui Xie}
+
+\begin{document}
+\maketitle
+
+'''
+
+latextIntro = r'''
+
+\section{Introduction}
+
+\begin{verbatim}
+A 3D V model to segment primary cancer ROI.
+  
+
+\end{verbatim}
+
+\section{Test Result Figures}
+
+'''
+
+
+latexTail = r'''
+
+\end{document}
+
+'''
+
+latexItem = r'''
+
+\begin{figure}
+	\begin{subfigure}{\linewidth}
+		\centering
+		\includegraphics[scale=0.7]{LabelFile}
+		\caption{Ground Truth+CT}
+	\end{subfigure}
+
+	\begin{subfigure}{\linewidth}
+		\centering
+		\includegraphics[scale=0.7]{CTFile}
+		\caption{CT}
+	\end{subfigure}
+
+	\begin{subfigure}{\linewidth}
+		\centering
+		\includegraphics[scale=0.7]{SegFile}
+    	\caption{Segmentation+CT}
+	\end{subfigure}
+
+\caption{\detokenize{PatientID_Slice}}
+\end{figure}
+
+\clearpage  % support huge number of figures
+
+'''
+
+# you may need to modify the output file directory and name
+outputLatex = r'''/home/hxie1/data/OvarianCancerCT/OCTestFor01_20190503.tex'''
+
+#imagesPath = r'''/home/hxie1/c-xwu000/data/OvarianCancerCT/Extract_uniform/segmented'''
+imagesPath = r'''/home/hxie1/data/OvarianCancerCT/Extract_uniform/segmented'''
+
+with open(outputLatex, "w") as f:
+    f.write(latexHead)
+    f.write(latextIntro)
+
+    originalCwd = os.getcwd()
+    os.chdir(imagesPath)
+    labelMergeList = [os.path.abspath(x) for x in os.listdir(imagesPath) if '_LabelMerge.png' in x]
+    os.chdir(originalCwd)
+
+    for labelFile in labelMergeList:
+        ctFile = labelFile.replace('_LabelMerge.png', '.png')
+        segFile = labelFile.replace('_LabelMerge.png', '_SegMerge.png')
+
+        basename = os.path.basename(ctFile)
+        patientID_Slice = basename[0: basename.find('.png')]
+        patientID_Slice = 'PatitenID_Slice: ' + patientID_Slice
+
+        item = latexItem.replace('LabelFile', labelFile).replace('CTFile', ctFile).replace('SegFile', segFile).replace('PatientID_Slice', patientID_Slice)
+        f.write(item)
+
+    f.write(latexTail)
+
+print(f'{outputLatex} has been outputed.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Tools/visualizeSegResult.py
+++ Tools/visualizeSegResult.py
@@ -7,17 +7,11 @@ import matplotlib.pyplot as plt
 sys.path.append("..")
 from FilesUtilities import *
 
-def getDiceFromArray(array, patientID):
-    nRows = array.shape[0]
-    for row in range(nRows):
-        if array[row, 0] == patientID:
-            return array[row, 1]
-
 def main():
     imageDir = "/home/hxie1/data/OvarianCancerCT/primaryROI/nrrd_npy"
     groundTruthDir = "/home/hxie1/data/OvarianCancerCT/primaryROI/labels_npy"
     predictDir = "/home/hxie1/data/OvarianCancerCT/primaryROI/predictionResult"
-    diceFile = os.path.join(predictDir, "predict_CV0_20190916_163347.txt")
+    diceFile = os.path.join(predictDir, "predict_CV0_20190916_163347.txt")   # need to modify
 
     suffix = ".npy"
     originalCwd = os.getcwd()
@@ -36,15 +30,11 @@ def main():
             line = line.replace('\t\t','\t')
             row = line.split('\t')
             if len(row) ==2:
-                ID_Dice[row[0]] = row[1]
+                ID_Dice[row[0]] = float(row[1])
 
 
     sliceIndices = [12,18, 25, 31, 37]
-
     nFigs = 0
-
-    flipAxis = (1,2)
-
     for file in filesList:
         patientID = getStemName(file, suffix)
         rawFilename = os.path.join(imageDir, patientID+suffix)
@@ -55,55 +45,29 @@ def main():
         gtImage = np.load(gtFilename).astype(np.float32)
         predictImage = np.load(predictFilename).astype(np.float32)
 
-        if patientID == "04029173":
-            slice = gtImage[25,]
-            f = plt.figure(1)
-            subplot1 = plt.subplot(1, 2, 1)
-            I = rawImage[5,]
-            subplot1.imshow(I, cmap='gray', vmin=np.amin(I), vmax=np.amax(I))
-            subplot1.set_title("Slice5")
-
-            subplot2 = plt.subplot(1, 2, 2)
-            I = rawImage[50,]
-            subplot2.imshow(I, cmap='gray', vmin=np.amin(I), vmax=np.amax(I))
-            subplot2.set_title("Slice50")
-
-            plt.show()
-            print("slice 50: ", slice[50,])
-            print("slice 100: ", slice[100,])
-            break;
-        else:
-            continue
-
         for s in sliceIndices:
             nFigs +=1
             f = plt.figure(nFigs)
             subplot1 = plt.subplot(2,2,1)
             I = rawImage[s,]
             subplot1.imshow(I, cmap='gray', vmin=np.amin(I), vmax=np.amax(I))
-            subplot1.set_title("rawImage")
-
-            rawMinAbs = np.abs(np.amin(I))
-            rawMaxAbs = np.abs(np.amax(I))
-            c = rawMinAbs+ rawMaxAbs  #label scale factor.
+            subplot1.set_title("Raw: "+ patientID+f"_s{s}")
 
             subplot2 = plt.subplot(2,2,2)
-            I = (1-gtImage[s,])*rawImage[s,] + gtImage[s,]*c
+            I = rawImage[s,] + gtImage[s,]
             subplot2.imshow(I, cmap='gray', vmin=np.amin(I), vmax=np.amax(I))
             subplot2.set_title("GT")
 
             subplot3 = plt.subplot(2,2,3)
-            I = (1- predictImage[s,])*rawImage[s,] + predictImage[s,]*c
+            I = rawImage[s,] + predictImage[s,]
             subplot3.imshow(I, cmap='gray', vmin=np.amin(I), vmax=np.amax(I))
             dice = float(ID_Dice[patientID])
-            subplot3.set_title(f"predict_Dice({dice: .2%})")
+            subplot3.set_title(f"Predict_Dice({dice: .2%})")
 
             subplot4 = plt.subplot(2,2,4)
-            I = (1-gtImage[s,])*rawImage[s,]*(1- predictImage[s,])+ (gtImage[s,]-predictImage[s,])*c
+            I = rawImage[s,]+ (gtImage[s,]-predictImage[s,])
             subplot4.imshow(I, cmap='gray', vmin=np.amin(I), vmax=np.amax(I))
             subplot4.set_title("GT-predict")
-
-            # f.suptitle(patientID + "_dice_0.98")
 
             plt.tight_layout()
 
