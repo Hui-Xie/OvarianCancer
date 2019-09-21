a24243c4a5c99f634c6a020b5f7f0e3720dcd96b
Hui Xie <sheenxh@gmail.com>
2019-09-21 12:48:36 -0500

    debug boundary loss2

--- CustomizedLoss.py
+++ CustomizedLoss.py
@@ -182,7 +182,7 @@ class BoundaryLoss2(_Loss):
                 # case5: Null (there is no prediction 1 and groundtruth 1, so no loss at all)
                 else:
                     continue
-
+               
 
             levelSetATensor = torch.from_numpy(levelSetA).float().to(inputx.device)
             levelSetBTensor = torch.from_numpy(levelSetB).float().to(inputx.device)
