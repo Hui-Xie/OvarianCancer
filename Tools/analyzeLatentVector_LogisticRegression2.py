720f5e595c6381433b59dde837d478a977d3d25a
Hui Xie <sheenxh@gmail.com>
2019-12-07 16:20:53 -0600

    prepare logistic curve for latent vector

--- Tools/analyzeLatentVector_LogisticRegression2.py
+++ Tools/analyzeLatentVector_LogisticRegression2.py
@@ -1,6 +1,6 @@
 # Analyze the correlation between latent vector and its corresponding response
 
-# this file is analyzing 3D latent vector(1536*3*3)
+# this file is analyzing 1D latent vector(1536*1 per patient)
 
 # dicesFilePath =  "/home/hxie1/data/OvarianCancerCT/primaryROI1_1_3/predictResult/20191023_153046/patientDice.json"
 # latentVectorDir =  "/home/hxie1/data/OvarianCancerCT/primaryROI1_1_3/latent/latent_20191023_153046"
@@ -12,7 +12,7 @@ patientResponsePath = "/home/hxie1/data/OvarianCancerCT/patientResponseDict.json
 aList = range(92,97,1)  #dice range 92% to 97%, step 1%
 diceThresholdList=[x/100 for x in aList]
 accuracyThreshold = 0.8  # for each feature
-F,H,W = 1536,3,3  #Features, Height, Width of latent vector
+F,W = 1536,1  #Features, Width of latent vector, per patient
 gpuDevice = 0   #GPU ID
 K = 10 # the top K maximum accuracy positions
 
@@ -48,7 +48,7 @@ def main():
         rawPatientDice = json.load(f)
     print(f"latent dir: {latentVectorDir}")
     print(f"There are total {len(rawPatientDice)} patients.")
-    print(f"Each patient has a latent vector of size {(F,H,W)}")
+    print(f"Each patient has a latent vector of size {(F,W)}")
     print(f"Each feature alone predicts its response through a Logistic regression along patients dimension.")
     print(f"accuracy Threshold = {accuracyThreshold}, for each feature")
 
@@ -60,6 +60,8 @@ def main():
     medianAccuracy = []
     maxAccuracy  = []
     numBestFeatures = []  # num of features whose predicted accuracy is greater than specific threshold
+
+    # For F*1 latent Vector
 
     for diceThreshold in diceThresholdList:
         patientDice = rawPatientDice.copy()
@@ -75,41 +77,41 @@ def main():
         averageDiceSamples.append(dices/N)
 
         # get response vector and assemble latent Vectors
-        X = np.empty((F, H, W, N), dtype=np.float)  # latent vectors
+        X = np.empty((F, N), dtype=np.float)  # latent vectors
         Y01 = np.empty((1, N), dtype=np.int)  # response in 0, 1 range
         for i, key in enumerate(list(patientDice)):
             Y01[0, i] = patientResponse[key]
             filePath = os.path.join(latentVectorDir, key + ".npy")
             V = np.load(filePath)
-            assert (F, H, W) == V.shape
-            X[:, :, :, i] = V
+            assert (F, 1) == V.shape
+            X[:,i] = V
         response1Rate.append(Y01.sum() / N)
         rawX = X.copy()
 
         # normalize latentV along patient dimension
-        mean = np.mean(X, axis=3, keepdims=True)
-        std = np.std(X, axis=3,keepdims=True)
-        mean = np.repeat(mean, N, axis=3)
-        std = np.repeat(std, N, axis=3)
+        mean = np.mean(X, axis=1, keepdims=True)
+        std = np.std(X, axis=1,keepdims=True)
+        mean = np.repeat(mean, N, axis=1)
+        std = np.repeat(std, N, axis=1)
         X = (X - mean) / std
 
-        # Analysis: logistic loss =\sum (-y*log(sigmoid(x))-(1-y)*log(1-sigmoid(x)))
-        # here W0 and W1 has a shape of (F,H, W)
+        # Logistic Regression Analysis: logistic loss =\sum (-y*log(sigmoid(x))-(1-y)*log(1-sigmoid(x)))
+        # here W0 and W1 each have a shape of (F,1)
         # sigmoid(x) = sigmoid(W0+W1*x)
         lr = 0.01
-        nIteration = 100
-        W0 = torch.zeros((F, H, W), dtype=torch.float, requires_grad=True, device=gpuDevice)
-        W1 = torch.zeros((F, H, W), dtype=torch.float, requires_grad=True, device=gpuDevice)
+        nIteration = 200
+        W0 = torch.zeros((F, 1), dtype=torch.float, requires_grad=True, device=gpuDevice)
+        W1 = torch.zeros((F, 1), dtype=torch.float, requires_grad=True, device=gpuDevice)
         W1.data.fill_(0.01)
         for _ in range(0, nIteration):
-            loss = torch.zeros((F, H, W), dtype=torch.float, device=gpuDevice)
+            loss = torch.zeros((F, 1), dtype=torch.float, device=gpuDevice)
             if W0.grad is not None:
                 W0.grad.data.zero_()
             if W1.grad is not None:
                 W1.grad.data.zero_()
             for i in range(0,N):
                 y = Y01[0,i]
-                x = torch.from_numpy(X[:,:,:,i]).type(torch.float32).to(gpuDevice)
+                x = torch.from_numpy(X[:,i]).type(torch.float32).to(gpuDevice)
                 sigmoidx = torch.sigmoid(W0+W1*x)
                 loss += -y*torch.log(sigmoidx)-(1-y)*torch.log(1-sigmoidx)
             loss = loss/N
@@ -123,13 +125,13 @@ def main():
         W0 = W0.detach().cpu().numpy()
         W1 = W1.detach().cpu().numpy()
 
-        W1Ex = np.reshape(np.repeat(W1, N, axis=2), X.shape)
-        W0Ex = np.reshape(np.repeat(W0, N, axis=2), X.shape)
+        W0Ex = np.repeat(W0, N, axis=1)
+        W1Ex = np.repeat(W1, N, axis=1)
 
         sigmoidX = 1.0 / (1.0 + np.exp(-(W0Ex + W1Ex * X)))
         predictX = (sigmoidX >= 0.5).astype(np.int)
-        Y = np.reshape(np.repeat(Y01, F * H * W, axis=0), X.shape)
-        accuracyX = ((predictX - Y) == 0).sum(axis=3) / N
+        Y = np.repeat(Y01, F, axis=0)
+        accuracyX = ((predictX - Y) == 0).sum(axis=1) / N
 
         minAccuracy.append(accuracyX.min())
         meanAccuracy.append(accuracyX.mean())
@@ -140,33 +142,36 @@ def main():
         numBestFeaturesTemp = accuracyBig.sum()
         numBestFeatures.append(numBestFeaturesTemp)
 
-        # Analyze the locations of top accuracies.
+        #  Analyze the locations of top accuracies.
         print(f"\nfinished dice {diceThreshold}...")
         k=K if numBestFeaturesTemp >= K else numBestFeaturesTemp
         print(f"Its top {k} accuracies location:")
-        accuracyXFlat = accuracyX.flatten()
-        topKIndicesFlat = np.argpartition(accuracyXFlat, kth=-k)[-k:]
-        topKIndices = np.unravel_index(topKIndicesFlat, accuracyX.shape)
+        accuracyXFlat = np.squeeze(accuracyX)
+        topKIndices = np.argpartition(accuracyXFlat, kth=-k)[-k:]
         print(f"indices:    {topKIndices}")
-        print(f"accuracies: {accuracyX[topKIndices]}")
+        print(f"accuracies: {accuracyXFlat[topKIndices]}")
 
-        #Analyze the feature value inside its feature map
-        rawXMean = np.mean(rawX, axis=3, keepdims=False)
-        rawXMax  = np.max(rawX, axis=3, keepdims=False)
-        rawXMin  = np.min(rawX, axis=3, keepdims=False)
+        #draw logistic regression curves
+        fig = plt.figure(int(diceThreshold*100+100))
+        colorList = ['b',  'g', 'r', 'c', 'm', 'y','k']
+            
+
         for i in range(0,k):
-            f = topKIndices[0][i]
-            y = topKIndices[1][i]
-            x = topKIndices[2][i]
-            print("====== Red value in below matrix is the highly positively relative with response ======.")
-            print(f"min map including index(f,y,x):({f},{y},{x}): ")
-            printFeatureMap(rawXMin[f,], (y, x))
-            print(f"mean map including index(f,y,x):({f},{y},{x}): ")
-            printFeatureMap(rawXMean[f,], (y,x))
-            print(f"max map including index(f,y,x):({f},{y},{x}): ")
-            printFeatureMap(rawXMax[f,], (y, x))
+            f = topKIndices[i] # feature index
+            #drawM: draw Matrix row 0 to 2: x, sigmoid(w0+w1*), y
+            drawM = np.zeros((3, N), dtype=np.float)
+            drawM[0,] = X[f,]
+            drawM[1,] = 1.0 / (1.0 + np.exp(-(W0[f] + W1[f] * X[f,])))
+            drawM[2,] = Y01
+            drawM = drawM[:, drawM[0,:].argsort()]
 
-
+            plt.plot(drawM[0,], drawM[2,], color=colorList[i], marker='x')
+            plt.plot(drawM[0,], drawM[1,], color=colorList[i])
+            plt.gca().set_ylim([0, 1.0])
+            plt.title(f"Top {k} accuracy logistic regression")
+            plt.xlabel('normalized latent value')
+            plt.ylabel('Response')
+            plt.savefig(os.path.join(latentVectorDir, f"logisticRegress_diceThreshold{int(diceThreshold*100)}.png"))
 
 
 
@@ -187,7 +192,7 @@ def main():
 
 
     #draw table:
-    f = plt.figure(1)
+    fig = plt.figure(2)
     plt.plot(diceThresholdList, minAccuracy)
     plt.plot(diceThresholdList, meanAccuracy)
     plt.plot(diceThresholdList, medianAccuracy)
@@ -201,7 +206,7 @@ def main():
     plt.ylabel('Prediction Accuracy')
     plt.savefig(os.path.join(latentVectorDir, f"SingleFeaturePrediction.png"))
 
-    f = plt.figure(2)
+    fig = plt.figure(3)
     plt.plot(diceThresholdList, rateBestFeatures)
     plt.gca().set_ylim([0, 1.0])
     plt.title(f"Rate of Best Features on different dice thresholds")
