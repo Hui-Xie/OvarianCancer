f59382b7ba80b38b036fbf83b247f7f10e205352
Hui Xie <sheenxh@gmail.com>
2019-12-24 15:52:53 -0600

    finish full feature experiment

--- Tools/solveCubicEquation.py
+++ Tools/solveCubicEquation.py
@@ -2,14 +2,24 @@
 #formula from: https://sciencing.com/how-to-factor-polynomials-of-degree-3-12751796.html
 # but this formula only gives one solution which may has 3 solution.
 
+# best solution:https://math.vanderbilt.edu/schectex/courses/cubic/
+
+'''
 a=1
 b=-6.0
 c=11.0
 d=-6
 # this cubic equation has 3 real root solution: 1,2,3
 
+'''
+a=1.0
+b =-270.0
+c = 23600.0-0.00002
+d = -660000.0+0.00001*170   # which get x = 109. good.
+
+
 def f(x):
-    return a*x**3+b*x**2+c*x+d
+    return a*(x**3)+b*(x**2)+c*x+d
 
 
 p =-b/(3*a)
@@ -21,6 +31,8 @@ x = (q+(q*q+(r-p*p)**3)**(1/2))**(1/3) + (q-(q*q+(r-p*p)**3)**(1/2))**(1/3) +p
 
 print(f"x= {x}")
 print(f"f(x)= {f(x)}")
+
+# it only get root x=3 with tiny virtual part.
 
 
 
