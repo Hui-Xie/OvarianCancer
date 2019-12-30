43a1b948f9b04ddc283c5cf6dde6eb3b0f51d228
Hui Xie <sheenxh@gmail.com>
2019-12-30 10:25:48 -0600

    slice Mu and variance computation

--- OCTMultiSurfaces/network/measurement.py
+++ OCTMultiSurfaces/network/measurement.py
@@ -16,20 +16,30 @@ def computeMuVariance(x):
 
     # square probability to strengthen the big probability, and to reduce variance
     # "The rich get richer and the poor get poorer"
-    P = torch.pow(x, 2).float()
+    P = torch.pow(x, 2).float16()
     PColSum = torch.sum(P, dim=-2, keepdim=True).expand(P.size())  # column means H direction
     P = P/PColSum
     del PColSum   # in order to free memory for further reuse.
+    torch.cuda.empty_cache()
 
     # compute mu
-    Y = torch.arange(H).view((H,1)).expand(P.size()).float().to(device)
-    mu = torch.sum(P*Y, dim=-2, keepdim=True)
+    Y = torch.arange(H).view((H,1)).expand(P.size()).short().to(device)
+    # mu = torch.sum(P*Y, dim=-2, keepdim=True)
+    # use slice method to comput P*Y
+    for b in range(B):
+        if 0==b:
+            PY = (P[b,]*Y[b,]).unsqueeze(dim=0)
+        else:
+            PY = torch.cat((PY, (P[b,]*Y[b,]).unsqueeze(dim=0)))
+    mu = torch.sum(PY, dim=-2, keepdim=True)
+    del PY  # hope to free memory.
+    torch.cuda.empty_cache()
 
     # compute sigma2 (variance)
     Mu = mu.expand(P.size())
 
     #sigma2 = torch.sum(P*torch.pow(Y-Mu,2), dim=-2,keepdim=False)
-    # this method is to avoid using big GPU memory .
+    # this slice method is to avoid using big GPU memory .
     for b in range(B):
         if 0==b:
             sigma2 = torch.sum(P[b,]*torch.pow(Y[b,]-Mu[b,],2), dim=-2,keepdim=False).unsqueeze(dim=0)
