9dfbcebee32534419cbd1a97ba07522e1790ca32
Hui Xie <sheenxh@gmail.com>
2019-12-31 16:58:28 -0600

    analyze the root of cubic equation

--- Tools/others/solveCubicEquation.py
+++ Tools/others/solveCubicEquation.py
@@ -43,16 +43,32 @@ def wolframeCubic(a, b, c, f):
     :param a: x_{i-1}
     :param b: x_i
     :param c: x_{i+1}
-    :param f: learningRate * barrierParameter
+    :param f: learningRate * barrierParameter(mu)
     :return:
     '''
+    S = 2 * a ** 3 - 3 * a ** 2 * b - 3 * a ** 2 * c
     P = -a**2 + a*b + a*c - b**2 + b*c - c**2 - 6*f
-    S = 2*a**3 - 3*a**2*b - 3*a**2*c
     T = - 3*a*b**2 + 12*a*b*c - 3*a*c**2 - 9*a*f + 2*b**3 - 3*b**2*c - 3*b*c**2 + 18*b*f + 2*c**3 - 9*c*f
-    Q = (S+(4*P*P*P+(S+T)*(S+T))**(1/2)+T)**(1.0/3.0)
-    xReal = -1.0/(6*2**(1.0/3))*Q+P/(Q*3*2**(2.0/3))+(a+b+c)/3.0
-    xImag = Q*3**(1/2)/(6*2**(1.0/3))+ P*3**(1/2)/(Q*3*2**(2.0/3))
-    return xReal, xImag
+    Q = (S+(4*P**3+(S+T)**2)**(1/2)+T)**(1.0/3.0)
+
+    x1 = 1.0 / (3 * 2 ** (1.0 / 3)) * Q - (2 ** (1.0 / 3) * P) / (3 * Q) + (a + b + c) / 3.0
+    x2 = -Q * (1 - 1j * 3 ** (1 / 2)) / (6 * 2 ** (1.0 / 3)) + P * (1 + 1j * 3 ** (1 / 2)) / (
+                Q * 3 * 2 ** (2.0 / 3)) + (a + b + c) / 3.0  # choice by wolframAlpha, as a<x<c
+    x3 = -Q * (1 + 1j * 3 ** (1 / 2)) / (6 * 2 ** (1.0 / 3)) + P * (1 - 1j * 3 ** (1 / 2)) / (
+                Q * 3 * 2 ** (2.0 / 3)) + (a + b + c) / 3.0
+    x3Real = -Q / (6 * 2 ** (1.0 / 3)) + P / (Q * 3 * 2 ** (2.0 / 3)) + (a + b + c) / 3.0
+    x3Imag = -Q * 3 ** (1 / 2) / (6 * 2 ** (1.0 / 3)) - P * 3 ** (1 / 2) / (
+                Q * 3 * 2 ** (2.0 / 3))
+
+
+    #xReal = -Q/(6*2**(1.0/3))+P/(Q*3*2**(2.0/3))+(a+b+c)/3.0
+    #xImag = Q*3**(1/2)/(6*2**(1.0/3))+ P*3**(1/2)/(Q*3*2**(2.0/3))
+
+    return x1,x2,x3, x3Real, x3Imag
+
+
+
+    #return x
 
 
 
@@ -84,7 +100,7 @@ def main():
 
 
     print("\n")
-    print("Verify a cubic equation whose solution should be 60-110 range, whith expection 100")
+    print("Verify a cubic cardano equation whose solution should be 60-110 range, with expectation 100")
     a = 1.0
     b = -270.0
     c = 23600.0 - 0.00002
@@ -100,37 +116,18 @@ def main():
     print("\n")
     print("Use WolframAlpha method")
     print("for a=60, b=100, c=110, f=0.01,  slove: solve { x^3-(a+b+c)*x^2+(a*b+a*c+b*c-2*f)*x+f*(a+c)-a*b*c==0  && x>a && x<c && 0.1>f>0 && a>0 && b>0  && c>0 } over the reals")
-    a =60
-    b =100
-    c =110
+    a =60.0
+    b =100.0
+    c =110.0
     f = 0.01
-    xReal, xImag = wolframeCubic(a,b,c,f)
-    print(f"xReal = {xReal}, xImag={xImag}")
+    #xReal, xImag, x = wolframeCubic(a,b,c,f)
+    # print(f"xReal = {xReal}, xImag={xImag}, x={x}")
+    x1, x2,x3, x3Real, x3Imag = wolframeCubic(a,b,c,f)
+    print (f"x1={x1}, x2={x2}, \n x3={x3},\n x3Real={x3Real}, x3Imag={x3Imag}")
 
     print ("======= The end ==========")
 
 if __name__ == "__main__":
-
-    '''
-    a =1
-    b=2
-    c =3
-    f =0.01
-    import math
-    sqrt = math.sqrt
-    I =1j
-
-
-    x = -((1 - I * sqrt(3))  * (2 * a ^ 3 - 3 * a ^ 2 * b - 3 * a ^ 2 * c + sqrt(4 * (-a ^ 2 + a * b + a * c - b ^ 2 + b * c - c ^ 2 - 6 * f) ^ 3
-                                                              + ( 2 * a ^ 3 - 3 * a ^ 2 * b - 3 * a ^ 2 * c - 3 * a * b ^ 2 + 12 * a * b * c - 3 * a * c ^ 2 - 9 * a * f + 2 * b ^ 3 - 3 * b ^ 2 * c - 3 * b * c ^ 2 + 18 * b * f + 2 * c ^ 3 - 9 * c * f) ^ 2) - 3 * a * b ^ 2 + 12 * a * b * c - 3 * a * c ^ 2 - 9 * a * f + 2 * b ^ 3 - 3 * b ^ 2 * c - 3 * b * c ^ 2 + 18 * b * f + 2 * c ^ 3 - 9 * c * f) ^ (
-                      1 / 3)) / (6 * 2 ^ (1 / 3)) \
-        + ( (1 + I * sqrt(3)) * (-a ^ 2 + a * b + a * c - b ^ 2 + b * c - c ^ 2 - 6 * f)) / (3 * 2 ^ (2 / 3)
-           * (2 * a ^ 3 - 3 * a ^ 2 * b - 3 * a ^ 2 * c + sqrt( 4 * (-a ^ 2 + a * b + a * c - b ^ 2 + b * c - c ^ 2 - 6 * f) ^ 3 + (
-                        2 * a ^ 3 - 3 * a ^ 2 * b - 3 * a ^ 2 * c - 3 * a * b ^ 2 + 12 * a * b * c - 3 * a * c ^ 2 - 9 * a * f + 2 * b ^ 3 - 3 * b ^ 2 * c - 3 * b * c ^ 2 + 18 * b * f + 2 * c ^ 3 - 9 * c * f) ^ 2)
-              - 3 * a * b ^ 2 + 12 * a * b * c - 3 * a * c ^ 2 - 9 * a * f + 2 * b ^ 3 - 3 * b ^ 2 * c - 3 * b * c ^ 2 + 18 * b * f + 2 * c ^ 3 - 9 * c * f) ^ (1 / 3))
-        + 1 / 3 * ( a + b + c)
-    
-    '''
-    main()
+     main()
 
 
