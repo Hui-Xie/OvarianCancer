d75fffffb4187760c10e01d4a9a47094bab1735c
Hui Xie <sheenxh@gmail.com>
2020-01-02 16:37:45 -0600

    start to debug ProximalIPM method

--- OCTMultiSurfaces/network/CVTrainMultiSurface.py
+++ OCTMultiSurfaces/network/CVTrainMultiSurface.py
@@ -181,7 +181,7 @@ def main():
             preLoss = validLoss
             netMgr.saveNet(netPath)
 
-        # print(f"finished epoch: {epoch}")
+        print(f"finished epoch: {epoch}")
 
     print("============ End of Cross valiation training for OCT Multisurface Network ===========")
 
