097692f4a32288a897f133956278a46e50ee2955
Hui Xie <sheenxh@gmail.com>
2020-01-04 11:04:04 -0600

    add ConfigParameter file for old network

--- Tools/others/addConfigParaFileForOldNetwork.py
+++ Tools/others/addConfigParaFileForOldNetwork.py
@@ -1,14 +1,14 @@
 
 # add config Parameter Dict file for old network
 
-netPath = "/home/hxie1/data/OCT_Beijing/numpy/10FoldCVForMultiSurfaceNet/netParameters/OCTUnet/expUnet_20191228_CV0"
+netPath = "/home/hxie1/data/OCT_Beijing/numpy/10FoldCVForMultiSurfaceNet/netParameters/OCTUnet/expUnet_20191228_CV7"
 
 import torch
 import os
 
 configParameterDict={}
-configParameterDict["validationLoss"] = 0.4109
-configParameterDict["epoch"] = 2992
+configParameterDict["validationLoss"] = 0.4763
+configParameterDict["epoch"] = 1949
 
 torch.save(configParameterDict, os.path.join(netPath, "ConfigParameters.pt"))
 
