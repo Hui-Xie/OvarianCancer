e982c39e2a414a4e9398a8e5c88f876637bb3605
Hui Xie <sheenxh@gmail.com>
2020-01-11 12:55:14 -0600

    use scatter and gather for tensor extract and assignment

--- OCTMultiSurfaces/network/OCTOptimization.py
+++ OCTMultiSurfaces/network/OCTOptimization.py
@@ -442,10 +442,10 @@ def getLIS_gpu(X):
     for inpupt X of size (B,S,W)
     https://en.wikipedia.org/wiki/Longest_increasing_subsequence
 
-    all index data use torch.long
+    all index use torch.long
 
     :param X:  of size(B,S,W)
-    :return: Tensor with choosing element in its location and non-choosing element marked as 0, same length with input X
+    :return: Tensor with choosing element in its location keeep original value, and non-choosing element marked as 0, same length with input X
     '''
 
     B,S,W = X.shape
@@ -457,37 +457,36 @@ def getLIS_gpu(X):
     # because j ≥ 1 represents the length of the increasing subsequence, and k ≥ 0 represents the index of its termination.
 
     L =  torch.zeros((B,W), dtype=torch.long, device=device) # length of each LIS
-    BIndex, WIndex = (L+1).nonzero(as_tuple=True)  # for flatted tuple as index, each tuple has length of N
-    N = B*W  # number of LISs
-
     for i in range(0,S):
         # Binary search for the largest positive j ≤ L such that X[M[j]] <= X[i]
         lo = torch.empty((B,W), dtype=torch.long, device=device).fill_(1)
-        hi = L.clone()
+        hi = L
         mid = lo.clone()
         while (lo <= hi).any():
             mid = torch.where(lo <= hi, torch.ceil((lo + hi) / 2.0).long(), mid)
-            X_M_mid = X[BIndex, mid.view(N), WIndex].view(B,W)  # X[M[mid]]
+            mid3D = mid.unsqueeze(dim=1)
+            X_M_mid = X.gather(1, mid3D).squeeze(dim=1)
             lo  = torch.where((X_M_mid <= X[:,i,:]) & (lo <= hi), mid+1, lo)
             hi  = torch.where((X_M_mid >  X[:,i,:]) & (lo <= hi), mid-1, hi)
 
         # After searching, lo is 1 greater than the length of the longest prefix of X[i]
         newL = lo
+        newL3D = newL.unsqueeze(dim=1)
 
         # The predecessor of X[i] is the last index of the subsequence of length newL - 1
-        P[:,i,:] = M[BIndex, (newL - 1).view(N), WIndex].view(B,W)
-        M[BIndex, newL.view(N), WIndex].fill_(i)  # save index of choosing element.
-        # https://discuss.pytorch.org/t/creating-a-mask-tensor-from-an-index-tensor/31648
+        P[:, i, :] = M.gather(1,newL3D-1).squeeze(dim=1)
+        M.scatter_(1, newL3D, i)
 
         # If we found a subsequence longer than any we've found yet, update L
         L = torch.where(newL>L, newL, L)
 
     # Reconstruct the longest increasing subsequence LIS
     LIS = torch.zeros_like(X)
-    k = M[BIndex, L.view(N), WIndex].view(N)
-    k0 = torch.zeros((B,W), dtype=torch.long, device=device).view(N) # indicate all initial index
+    L3D = L.unsqueeze(dim=1)
+    k = M.gather(1,L3D)  # both k and k0 are 3D tensor.
+    k0 = torch.zeros((B,1, W), dtype=torch.long, device=device) # indicate all initial index
     while k.bool().any():
-        LIS[BIndex, k, WIndex] = torch.where(k.bool(), X[BIndex, k, WIndex], LIS[BIndex, k, WIndex])
-        k = torch.where(k.bool(), P[BIndex, k, WIndex], k0)
+        LIS.scatter_(1,k, torch.where(k.bool(), X.gather(1,k), LIS))
+        k = torch.where(k.bool(), P.gather(1,k), k0)
 
     return LIS
