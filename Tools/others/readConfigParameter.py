657d2c04afc3fbd75e666456e2cfa1e0a512bd55
Hui Xie <sheenxh@gmail.com>
2020-01-17 14:18:51 -0600

    add read config parameters

---
+++ Tools/others/readConfigParameter.py
@@ -0,0 +1,8 @@
+
+import torch
+
+configFile = "/home/hxie1/data/OCT_Beijing/numpy/10FoldCVForMultiSurfaceNet/netParameters/OCTUnet/expUnet_20200117_CV1_DP/ConfigParameters.pt"
+
+configDict = torch.load(configFile)
+
+print(configDict)
