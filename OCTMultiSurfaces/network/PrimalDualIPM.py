ef0d26bfed501689dee402c66626f9dd16a0fffe
Sheen <sheenxh@gmail.com>
2020-01-18 10:53:28 -0600

    rewrite IPM interface function

--- OCTMultiSurfaces/network/PrimalDualIPM.py
+++ OCTMultiSurfaces/network/PrimalDualIPM.py
@@ -6,12 +6,20 @@ class SeparationPrimalDualIPMFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, Mu, Sigma2Reciprocal, A, S0, Lamda, alpha, epsilon):
         '''
-        S* = arg min_{S} {(S-Mu)' *Q* (S_Mu)/2}, such that A <= 0
+        S* = arg min_{S} {(S-Mu)' *Q* (S-Mu)/2}, such that A <= 0
 
         :param ctx: context object
-        :param Mu: mean in (B,NumSurfaces, W) size
-        :param Sigma2Reciprocal: the reciprocal of variance in (B,NumSurfaces, W) size
-        :param A: constraint matrix A <= 0 with A of size(n-1, n)
+        :param Mu: predicted mean,  in (B,W, NumSurfaces) size, below N=NumSurfaces
+        :param Sigma2Reciprocal: Q, the diagonal reciprocal of variance in (B,W,N,N) size
+        :param A: constraint matrix A <= 0 with A of size(n-1, n), in (B, W, N-1,N) size
+        :param S0: in (B,W, N) size
+        :param Lamda: dual variable, in (B,W,N-1) size
+        :param alpha: IPM iteration t enlarge variable, in (B,W) size
+        :param epsilon: float scalar, error tolerance
+
+
+
+
         :return: SOptimal: the optimal S, surface location in (B,NumSurfaces, W) size
                  MInverse:  (2n-1)*(2n-1) matrix,where n= Numsurfaces
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# OvarianCancer
-OvarianCancer
+# DeepLearningSegmentation
+
+
