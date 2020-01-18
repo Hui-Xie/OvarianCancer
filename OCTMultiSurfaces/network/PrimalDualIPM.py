6172cf3f46952f76998f4fa93948da0a0630a493
Sheen <sheenxh@gmail.com>
2020-01-18 13:03:18 -0600

    code IPM method

--- OCTMultiSurfaces/network/PrimalDualIPM.py
+++ OCTMultiSurfaces/network/PrimalDualIPM.py
@@ -3,43 +3,68 @@ import torch
 import torch.nn as nn
 
 class SeparationPrimalDualIPMFunction(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, Mu, Sigma2Reciprocal, A, S0, Lamda, alpha, epsilon):
-        '''
-        S* = arg min_{S} {(S-Mu)' *Q* (S-Mu)/2}, such that A <= 0
-
-        :param ctx: context object
-        :param Mu: predicted mean,  in (B,W, NumSurfaces) size, below N=NumSurfaces
-        :param Sigma2Reciprocal: Q, the diagonal reciprocal of variance in (B,W,N,N) size
-        :param A: constraint matrix A <= 0 with A of size(n-1, n), in (B, W, N-1,N) size
-        :param S0: in (B,W, N) size
-        :param Lamda: dual variable, in (B,W,N-1) size
-        :param alpha: IPM iteration t enlarge variable, in (B,W) size
-        :param epsilon: float scalar, error tolerance
-
-
-
-
-        :return: SOptimal: the optimal S, surface location in (B,NumSurfaces, W) size
-                 MInverse:  (2n-1)*(2n-1) matrix,where n= Numsurfaces
-
-        '''
-        Q = Sigma2Reciprocal
-        assert Mu.shape == S0.shape
-        B,N, W = Mu.size() # N is numSurfaces
-        assert alpha > 1 and epsilon >0
-        assert N-1, 1 == Lamda.size()
-        S = S0
-        while True:
-            R =  torch.bmm(Q,S-Mu)+torch.bmm(torch.transpose(A,1,2), Lamda)
-
-
-        pass
-
 
     @staticmethod
     def backward(ctx,dL):
         pass
+
+
+def forward(ctx, Mu, Q, A, S0, Lambda, alpha, epsilon):
+    '''
+    S* = arg min_{S} {(S-Mu)' *Q* (S-Mu)/2}, such that A <= 0
+
+    :param ctx: context object
+    :param Mu: predicted mean,  in (B,W, NumSurfaces) size, in below N=NumSurfaces
+    :param Q: Sigma2Reciprocal: Q, the diagonal reciprocal of variance in (B,W,N,N) size
+    :param A: constraint matrix A <= 0 with A of size(n-1, n), in (B, W, N-1,N) size
+    :param S0: in (B,W, N) size
+    :param Lambda: dual variable, in (B,W,N-1) size
+    :param alpha: IPM iteration t enlarge variable, in (B,W) size, alpha >1
+    :param epsilon: float scalar, error tolerance
+
+    :return: S: the optimal S, surface location in (B,NumSurfaces, W) size
+             MInverse:  (2n-1)*(2n-1) matrix,where n= NumSurfaces, save for backward
+
+    '''
+    assert Mu.shape == S0.shape
+    B,W,N = Mu.size() # N is numSurfaces
+    assert torch.all(alpha > 1) and epsilon >0
+    Mu = Mu.unsqueeze(dim=-1)  # in size: B,W,N,1
+    S0 = S0.unsqueeze(dim=-1)  # in size:B,W,N,1
+    Lambda = Lambda.unsqueeze(dim=-1) # in size: B,W, N-1,1
+    alpha = alpha.unsqueeze(dim=-1).unsqueeze(dim=-1) # in size: B,W,1,1
+    S = S0
+    Ones = torch.ones(B,W,N-1,1)
+
+    while True:
+        AS = torch.matmul(A,S)
+        DLambda = -torch.diag_embed(Lambda.squeeze(dim=-1))  # negative diagonal Lambda
+
+        # update t
+        t = -alpha*(N-1) / torch.matmul(AS.transpose(-1,-2), Lambda)    # t is in (B,W, 1,1) size
+
+        # compute primal dual search direction
+        R1 = torch.matmul(Q,S-Mu)+torch.matmul(torch.transpose(A,-1,-2), Lambda) # the upper part of residual matrix R, in size:B,W,N,1
+        R2 = torch.matmul(DLambda,AS)- Ones/t.expand_as(Ones)  # the lower part of residual matrixt R, in size: B,W, N-1,1
+        R = torch.cat((R1,R2),dim=-2) # in size: B,W, 2N-1,1
+        M = torch.cat(
+            (torch.cat((Q, A.transpose(-1,-2)), dim=-1),
+            torch.cat((torch.matmul(DLambda, A), -torch.diag_embed(AS.squeeze(dim=-1))), dim=-1)),
+            dim=-2)
+        MInv = torch.inverse(M)
+        PD = -torch.matmul(MInv,R) # primal dual improve direction
+
+        # linear search to determine step
+
+
+
+
+
+
+
+
+    pass
+
 
 class SeparationPrimalDualIPM(nn.Module):
     def __init__(self):
