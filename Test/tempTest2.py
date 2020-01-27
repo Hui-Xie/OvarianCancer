d2703ecf5802822941275b03ba6080c2578c6bc4
Hui Xie <sheenxh@gmail.com>
2020-01-27 14:33:23 -0600

    add a test code

---
+++ Test/tempTest2.py
@@ -0,0 +1,21 @@
+import torch
+from torch import autograd
+
+class Foo(autograd.Function):
+    @staticmethod
+    def forward(ctx, x):
+        s = x[0:10]
+        # s =x.clone()[0:10] # if we  use this line to replace its above line, error disappears
+        ctx.save_for_backward(x,s)
+        return s
+
+    @staticmethod
+    def backward(ctx, gx):
+        x,s = ctx.saved_tensors
+        return s.clone()
+
+inp = torch.rand(100, requires_grad=True)
+
+with torch.no_grad():
+    Foo.apply(inp).sum()
+print("Ok")
