b767e4141def9bba33b52f72c903a9299d33be13
Hui Xie <sheenxh@gmail.com>
2020-01-28 17:06:11 -0600

    OCT_JHU prepare

--- OCTMultiSurfaces/dataPrepareJHU/readVolFile.py
+++ OCTMultiSurfaces/dataPrepareJHU/readVolFile.py
@@ -2,12 +2,21 @@ import sys
 sys.path.append("/home/hxie1/.local/lib/python3.6/site-packages")
 import heyexReader
 
-volFilePath  = "/home/hxie1/data/OCT_JHU/OCT_Manual_Delineations-2018_June_29/vol/hc01_spectralis_macula_v1_s1_R.vol"
+from scipy.io import loadmat
 
+volFilePath  = "/home/hxie1/data/OCT_JHU/OCT_Manual_Delineations-2018_June_29/vol/hc01_spectralis_macula_v1_s1_R.vol"
+#matFilePath =  "/home/hxie1/data/OCT_JHU/OCT_Manual_Delineations-2018_June_29/delineation/hc01_spectralis_macula_v1_s1_R.mat"
+matFilePath = "/localscratch/Users/hxie1/temp/example.mat"
 vol = heyexReader.volFile(volFilePath)
 
-vol.renderIRslo("slo.png", renderGrid = True)
-vol.renderOCTscans("oct", renderSeg = True)
 
+# vol.renderIRslo("slo.png", renderGrid = True)  # this line will generate SLO image.
+# vol.renderOCTscans("oct", renderSeg = True)  # this line will generate a lot of BScans.
+
+seg = loadmat(matFilePath)
+
+segData =seg['control_pts']
+a = segData[0,0]
+b = segData[0,3]
 print(vol.oct.shape)
 print(vol.irslo.shape)
