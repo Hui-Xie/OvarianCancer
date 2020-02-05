07d6d06f3cc676b5dc5a0b69d6543915dfe0d400
Hui Xie <sheenxh@gmail.com>
2020-02-05 16:35:44 -0600

    pass debug of getLayerLabels

--- OCTMultiSurfaces/network/OCTDataSet.py
+++ OCTMultiSurfaces/network/OCTDataSet.py
@@ -3,7 +3,47 @@ import numpy as np
 import json
 import math
 import torch
-import torchvision.transforms as TF
+# import torchvision.transforms as TF
+
+
+def gaussianizeLabels(rawLabels, sigma, H):
+    '''
+    input: tensor(Num_surface, W)
+    output: tensor(Num_surace, H, W)
+    '''
+    device = rawLabels.device
+    Mu = rawLabels
+    Num_Surface, W = Mu.shape
+    Mu = Mu.unsqueeze(dim=-2)
+    Mu = Mu.expand((Num_Surface, H, W))
+    X = torch.arange(start=0.0, end=H, step=1.0).view((H, 1)).to(device, dtype=torch.float32)
+    X = X.expand((Num_Surface, H, W))
+    pi = math.pi
+    G = torch.exp(-(X - Mu) ** 2 / (2 * sigma * sigma)) / (sigma * math.sqrt(2 * pi))
+    return G
+
+
+def getLayerLabels(surfaceLabels, height):
+    '''
+
+    :param surfaceLabels: float tensor in size of (N,W) where N is the number of surfaces.
+            height: original image height
+    :return: layerLabels: long tensor in size of (H, W) in which each element is long integer  of [0,N] indicating belonging layer
+
+    '''
+    H = height
+    device = surfaceLabels.device
+    N, W = surfaceLabels.shape  # N is the number of surface
+    layerLabels = torch.zeros((H, W), dtype=torch.long, device=device)
+    surfaceLabels = (surfaceLabels + 0.5).long()  # let surface height match grid
+    surfaceCodes = torch.tensor(range(1, N + 1)).unsqueeze(dim=-1).expand_as(surfaceLabels)
+    layerLabels.scatter_(0, surfaceLabels, surfaceCodes)
+
+    for i in range(1,H):
+        layerLabels[i,:] = torch.where(0 == layerLabels[i,:], layerLabels[i-1,:], layerLabels[i,:])
+
+    return layerLabels
+
 
 class OCTDataSet(data.Dataset):
     def __init__(self, imagesPath, labelPath, IDPath, transform=None, device=None, sigma=20.0):
@@ -32,46 +72,10 @@ class OCTDataSet(data.Dataset):
             data = self.m_transform(data)
         result = {"images": data.unsqueeze(dim=0),
                   "GTs": self.m_labels[index,],
-                  "gaussianGTs": self.gaussianizeLabels(self.m_labels[index,], self.m_sigma, H),
+                  "gaussianGTs": gaussianizeLabels(self.m_labels[index,], self.m_sigma, H),
                   "IDs": self.m_IDs[str(index)]}
         return result
 
-    def gaussianizeLabels(self, rawLabels, sigma, H):
-        '''
-        input: tensor(Num_surface, W)
-        output: tensor(Num_surace, H, W)
-        '''
-        Mu = rawLabels
-        Num_Surface, W = Mu.shape
-        Mu = Mu.unsqueeze(dim=-2)
-        Mu = Mu.expand((Num_Surface,H,W))
-        X = torch.arange(start=0.0, end=H, step=1.0).view((H,1)).to(self.m_device, dtype=torch.float32)
-        X = X.expand((Num_Surface,H,W))
-        pi = math.pi
-        G  = torch.exp(-(X-Mu)**2/(2*sigma*sigma))/(sigma*math.sqrt(2*pi))
-        return G
-
-    def getLayersLabel(self, surfaceLabels, height):
-        '''
-
-        :param surfaceLabels: float tensor in size of (N,W) where N is the number of surfaces.
-                height: original image height
-        :return: layerLabels: long tensor in size of (H, W) in which each element is long integer  of [0,N] indicating belonging layer
-
-        '''
-        H = height
-        device = surfaceLabels.device
-        N,W = surfaceLabels.shape # N is the number of surface
-        layerLabels = torch.zeros((H, W), dtype=torch.long, device=device)
-        surfaceLabels = (surfaceLabels + 0.5).long() # let surface height match grid
-        surfacesCode = torch.tensor(range(1,N+1)).unsqueeze(dim=-1).expand_as(surfaceLabels)
-        layerLabels = torch.scatter(0, surfaceLabels, surfacesCode)
-        curveLoc = surfaceLabels[0,:]+1
-        bottom = torch.ones((1,W),  dtype=torch.long, device=device)*H
-        zeroWidth = torch.zeros((1,W), dtype=torch.long, device=device)
-        while torch.any(curveLoc < bottom):
-            layerLabels[curveLoc,:] = torch.where(zeroWidth == layerLabels[curveLoc,:],  layerLabels[curveLoc-1,:], layerLabels[curveLoc,:])
-            curveLoc = torch.where(curveLoc < bottom, curveLoc+1, curveLoc)
 
 
 
---
+++ Test/testGetLayersLabel.py
@@ -0,0 +1,17 @@
+import torch
+import sys
+sys.path.append("../OCTMultiSurfaces/network")
+from OCTDataSet import *
+
+
+def main():
+    surfaces = torch.tensor([[3.5, 4, 7, 10],[2, 3, 6, 8],[4, 5, 9, 10]]).t()
+    Height = 15
+    print(f"surfaces=\n{surfaces}")
+    layers = getLayerLabels(surfaces,Height)
+    print(f"layer=\n{layers}")
+
+
+
+if __name__ == "__main__":
+    main()
