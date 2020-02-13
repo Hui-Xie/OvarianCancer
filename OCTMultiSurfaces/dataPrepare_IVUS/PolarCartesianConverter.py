7ad2647e6b7f76ff0e4dc47c5b57e1b10eaa457e
Sheen <sheenxh@gmail.com>
2020-02-13 10:46:31 -0600

    add label polar cartesian converter interfaces

--- OCTMultiSurfaces/dataPrepare_IVUS/PolarCartesianConverter.py
+++ OCTMultiSurfaces/dataPrepare_IVUS/PolarCartesianConverter.py
@@ -17,6 +17,27 @@ class PolarCartesianConverter():
         self.rMax = rMax
         self.tMax = tMax
 
+    def cartesianLabel2Polar(self, cartesianLabel, rotation=0):
+        '''
+
+        :param cartesianLabel: C*N*2, where C is the number of class labels, N is number of points, 2 is 2 coordinates x and y;
+        :param rotation: rotation angular in integer degree of (0,360)
+        :return: polarLabel: (t,r) in size (C,N,2)
+
+        '''
+
+        x = cartesianLabel[:, :, 0] - self.centerx
+        y = cartesianLabel[:, :, 1] - self.centery
+        r = np.flip(np.sqrt(x ** 2 + y ** 2), axis=1)  # size: C,N, flip because warpPolar is clockwise direction to warp.
+        t = (np.arctan2(y, x) + 2 * np.pi) % (2 * np.pi)  # size: C,N
+        t = t * 360 / (2 * np.pi)
+
+        rotation = rotation % 360
+        if 0 != rotation:
+            r = np.roll(r, -rotation, axis=1)
+        polarLabel = np.concatenate((np.expand_dims(t, axis=-1), np.expand_dims(r, axis=-1)), axis=-1)
+        return polarLabel
+
     def cartesianImageLabel2Polar(self, cartesianImage, cartesianLabel, rotation=0):
         '''
 
@@ -31,34 +52,33 @@ class PolarCartesianConverter():
         polarImageSize = (self.rMax, self.tMax)
         polarImage = cv2.warpPolar(cartesianImage,polarImageSize, (self.centerx,self.centery), self.rMax, flags=cv2.WARP_FILL_OUTLIERS)
         polarImage = cv2.rotate(polarImage, cv2.ROTATE_90_CLOCKWISE)
-        x = cartesianLabel[:, :, 0] - self.centerx
-        y = cartesianLabel[:, :, 1] - self.centery
-        r = np.flip(np.sqrt(x**2 + y**2),axis=1) # size: C,N, flip because warpPolar is clockwise direction to warp.
-        t = (np.arctan2(y,x)+2*np.pi)%(2*np.pi)  # size: C,N
-        t = t*360/(2*np.pi)
-
-        rotation = rotation%360
+        rotation = rotation % 360
         if 0 != rotation:
             polarImage = np.roll(polarImage, rotation, axis=1)
-            r = np.roll(r, -rotation, axis=1)
-        polarLabel = np.concatenate((np.expand_dims(t, axis=-1),np.expand_dims(r,axis=-1)), axis=-1)
+        polarLabel = self.cartesianLabel2Polar(cartesianLabel, rotation)
         return polarImage, polarLabel
 
+    def polarLabel2Cartesian(self, polarLabel, rotation=0):
+        r = polarLabel[:, :, 1]  # size:C*N
+        t = polarLabel[:, :, 0]  # size:C*N
+        rotation = rotation % 360
+        if 0 != rotation:
+            r = np.roll(r, rotation, axis=1)
+        t = t * 2 * np.pi / 360
+        r = np.flip(r, axis=1)
+        x = r * np.cos(t) + self.centerx
+        y = r * np.sin(t) + self.centery
+        cartesianLabel = np.concatenate((np.expand_dims(x, axis=-1), np.expand_dims(y, axis=-1)), axis=-1)
+        return cartesianLabel
+
     def polarImageLabel2Cartesian(self, polarImage, polarLabel, rotation=0):
-        r = polarLabel[:, :, 1] # size:C*N
-        t = polarLabel[:, :, 0] # size:C*N
         rotation = rotation % 360
         if 0 != rotation:
             polarImage = np.roll(polarImage, -rotation, axis=1)
-            r = np.roll(r, rotation, axis=1)
-        t = t*2*np.pi/360
-        r = np.flip(r, axis=1)
-        x = r*np.cos(t) + self.centerx
-        y = r*np.sin(t) + self.centery
         polarImage = cv2.rotate(polarImage, cv2.ROTATE_90_COUNTERCLOCKWISE)
         cartesianmage = cv2.warpPolar(polarImage, self.cartesianImageShape, (self.centerx, self.centery), self.rMax,
                                    flags=cv2.WARP_FILL_OUTLIERS + cv2.WARP_INVERSE_MAP)
-        cartesianLabel= np.concatenate((np.expand_dims(x,axis=-1), np.expand_dims(y,axis=-1)), axis=-1)
+        cartesianLabel= self.polarLabel2Cartesian(polarLabel, rotation)
         return cartesianmage, cartesianLabel
 
 
---
+++ OCTMultiSurfaces/dataPrepare_IVUS/checkLayerConstraints.py
@@ -0,0 +1,5 @@
+# check all layer constraints
+
+segDir = "/home/hxie1/data/IVUS/Training_Set/Data_set_B/LABELS"
+
+
