fbdeb39553dccb90f3caf495efd7cc55c0df4ded
Hui Xie <sheenxh@gmail.com>
2020-02-14 09:41:08 -0600

    test verify polar numpy package

--- OCTMultiSurfaces/dataPrepare_IVUS/verifyPolarNumpyPack.py
+++ OCTMultiSurfaces/dataPrepare_IVUS/verifyPolarNumpyPack.py
@@ -1,6 +1,6 @@
 
 
-dataSetDir = "/home/hxie1/data/IVUS/polarNumpy/training"
+dataSetDir = "/home/hxie1/data/IVUS/polarNumpy/validation"
 outputDir = "/home/hxie1/temp"
 
 import numpy as np
@@ -19,7 +19,7 @@ W = W1
 with open(os.path.join(dataSetDir,"patientID.json")) as json_file:
     patientIDs = json.load(json_file)
 
-k= 51 # random test number
+k= 4 # random test number
 
 patientID = os.path.splitext(os.path.basename(patientIDs[str(k)]))[0]
 
