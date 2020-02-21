aa678607ea442d7103b574f3efe4305454cd5a63
Hui Xie <sheenxh@gmail.com>
2020-02-21 10:23:24 -0600

    square gaussian is a still Gaussian with bigger peak prob and less stdev

--- OCTMultiSurfaces/dataPrepare_IVUS/gausssianDist2.m
+++ OCTMultiSurfaces/dataPrepare_IVUS/gausssianDist2.m
@@ -1,11 +1,15 @@
 % research Gaussian distribution and its square distribution.
 x =[-4:0.1:4];
 y1 = normpdf(x,0,1);
-y2 = y1.*y1
-y2sum= sum(y2)
-y2 = y2./y2sum
+y1sum = sum(y1);
+y1 = y1./y1sum;
+
+y2 = y1.*y1;
+y2sum= sum(y2);
+y2 = y2./y2sum;
 
 figure
-plot(x,y1,x,y2)
-% square of gaussian distribution then normlize is not a gaussian distribution again, 
-% with less peak prob and less standard deviation. 
+plot(x,y1,"r-",x,y2,"g*")
+% conclusion:
+% square of gaussian distribution then normlize is still a gaussian distribution again, 
+% with bigger peak prob and less standard deviation. 
