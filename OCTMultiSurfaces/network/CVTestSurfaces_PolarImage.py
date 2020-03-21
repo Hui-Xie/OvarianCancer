9efb6f317895c915f45571fab0a2f88c89f74d83
Sheen <sheenxh@gmail.com>
2020-03-20 20:40:06 -0500

    change switch

--- OCTMultiSurfaces/network/CVTestSurfaces_PolarImage.py
+++ OCTMultiSurfaces/network/CVTestSurfaces_PolarImage.py
@@ -72,7 +72,7 @@ def main():
     # debug:
     MarkGTDisorder = False
     MarkPredictDisorder = False
-    OutputPredictImages = False
+    OutputPredictImages = True
     Output2Images = True
 
     # parse config file
