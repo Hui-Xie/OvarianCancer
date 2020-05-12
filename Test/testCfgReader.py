8681afa9eaf565aaf62dc2296748a058c2a21f8d
Hui Xie <sheenxh@gmail.com>
2020-05-12 11:56:58 -0500

    add dictionary config in yaml file

--- Test/testCfgReader.py
+++ Test/testCfgReader.py
@@ -6,7 +6,7 @@ import os
 
 
 
-cfgFilePath = "/home/sheen/Projects/DeepLearningSeg/OCTMultiSurfaces/testConfig/expTongren_10Surfaces_20200509/expTongren_10Surfaces_20200509_CV0.yaml"
+cfgFilePath = "/home/hxie1/Projects/DeepLearningSeg/OCTMultiSurfaces/testConfig/expTongren_10Surfaces_20200509/expTongren_10Surfaces_20200509_CV0.yaml"
 
 hps = ConfigReader(cfgFilePath)
 
@@ -17,9 +17,10 @@ hps.plane = 3
 
 print(f"hps.plane = {hps.plane}")
 
+goodBscans= hps.goodBscans
+#print(goodBscans['2639'])
 
-
-with open("/home/sheen/temp/output_test.txt", "w") as file:
+with open("/home/hxie1/temp/output_test.txt", "w") as file:
     hps.printTo(file)
 
 
