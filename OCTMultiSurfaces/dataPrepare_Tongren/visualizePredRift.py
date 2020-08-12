57069919a66bf98b9f3087b4327b61993b35ca06
Sheen <sheenxh@gmail.com>
2020-08-12 17:27:24 -0500

    visualizePredictRfit

--- OCTMultiSurfaces/dataPrepare_Tongren/visualizePredRift.py
+++ OCTMultiSurfaces/dataPrepare_Tongren/visualizePredRift.py
@@ -16,7 +16,8 @@ surfacesPath = srcNumpyDir + "/surfaces_CV0.npy"
 pltColors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple',  'tab:olive', 'tab:brown', 'tab:pink', 'tab:red', 'tab:cyan', 'tab:blue']
 
 
-index = 39 # /home/hxie1/data/OCT_Tongren/control/4511_OD_29134_Volume/20110629044120_OCT09.jpg\n
+# index = 39 # /home/hxie1/data/OCT_Tongren/control/4511_OD_29134_Volume/20110629044120_OCT09.jpg\n
+index = 15  #70
 s = 3  # surface N
 sp1 = 4 # surface N+1
 
@@ -31,6 +32,8 @@ import sys
 sys.path.append("../network")
 from OCTAugmentation import smoothCMA
 import torch
+sys.path.append(".")
+from TongrenFileUtilities import getSurfacesArray
 
 
 # find file
@@ -43,12 +46,14 @@ surfaces = np.load(surfacesPath) #BxNxW
 N = surfaces.shape[1]
 Rs = np.load(testRPath)
 
-
-
 a = IDList[index] # srcImagePath
 volume = a[a.find("control/")+8: a.rfind("/")]
 sliceID = a[-10: -5]
+sliceIndex = int(sliceID[-2:])-1
 outputVisaulRPath = predictOutputDir +f"/{volume}_{sliceID}_GT_R_Predict.png"
+xmlFilePath = predictOutputDir +"/xml"+ f"/{volume}_Sequence_Surfaces_Prediction.xml"
+predictS = getSurfacesArray(xmlFilePath)
+
 # read ground truth
 image = images[index,]
 surface = surfaces[index,]
@@ -74,6 +79,23 @@ subplot1.plot(range(0, W), surface[s, :].squeeze(), pltColors[s], linewidth=0.9)
 subplot1.plot(range(0, W), surface[sp1, :].squeeze(), pltColors[sp1], linewidth=0.9)
 subplot1.legend([f"g_{s}", f"g_{sp1}"], loc='lower center', ncol=2)
 subplot1.axis('off')
+
+subplot2 = plt.subplot(subplotRow, subplotCol, 2, facecolor='yellow')
+subplot2.plot(range(0, W), Rgt[s, :].squeeze(), pltColors[0], linewidth=1)
+subplot2.plot(range(0, W), RgtSmooth[s, :].squeeze(), pltColors[1], linewidth=1)
+subplot2.plot(range(0, W), RPredict[s, :].squeeze(), pltColors[2], linewidth=1)
+subplot2.plot(range(0, W), RPredictSmooth[s, :].squeeze(), pltColors[7], linewidth=1)
+# subplot2.set_ylim(0, max(max(Rgt[s, :]), max(RPredict[s,:])))
+subplot2.legend([f"Rgt_{s}", f"RgtSmooth_{s}",f"RPredict_{s}", f"RPredictSmooth_{s}"], loc='lower center', ncol=2)
+subplot2.axis('off')
+
+subplot3 = plt.subplot(subplotRow, subplotCol, 3)
+subplot3.imshow(image, cmap='gray')
+subplot3.plot(range(0, W), predictS[sliceIndex, s, :].squeeze(), pltColors[s], linewidth=0.9)
+subplot3.plot(range(0, W), predictS[sliceIndex, s, :].squeeze() + RPredict[s,:].squeeze(), pltColors[7], linewidth=0.9)
+subplot3.plot(range(0, W), predictS[sliceIndex, sp1, :].squeeze(), pltColors[sp1], linewidth=0.9)
+subplot3.legend([f"predict_{s}", f"predict_{s}+R_{s}", f"predict_{sp1}"], loc='lower center', ncol=3)
+subplot3.axis('off')
 
 plt.savefig(outputVisaulRPath, dpi='figure', bbox_inches='tight', pad_inches=0)
 plt.close()
