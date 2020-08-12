1b03d90a47a19d76267face923a21769591679a9
Sheen <sheenxh@gmail.com>
2020-08-12 11:53:42 -0500

    perpare output R compare

--- OCTMultiSurfaces/dataPrepare_Tongren/visualizePredRift.py
+++ OCTMultiSurfaces/dataPrepare_Tongren/visualizePredRift.py
@@ -1,8 +1,10 @@
 
-predictOutputDir = ""
-srcNumpyDir = ""
+predictOutputDir = "/home/hxie1/data/OCT_Tongren/numpy/10FoldCVForMultiSurfaceNet/log/SurfacesUnet/expTongren_9Surfaces_SoftConst_20200808_CV0_8Grad_LearnPair_Pretrain_LR1/testResult/realtime"
+srcNumpyDir = "/home/hxie1/data/OCT_Tongren/numpy/10FoldCVForMultiSurfaceNet/test"
+# only CV0 for this test.
+#  images_CV0.npy,  surfaces_CV0.npy,  patientID_CV0.json
 
-test
+testRPath = predictOutputDir + "/testR.npy"
 
 numPatients = 8
 numSlices = 31
