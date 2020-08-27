b081438d5ef293d9896a3d34f6865522b9862b4e
Sheen <sheenxh@gmail.com>
2020-08-27 17:49:08 -0500

    add lambda test

---
+++ OCTMultiSurfaces/SoftSepar3Unet/LambdaSubnet_Test.py
@@ -0,0 +1,109 @@
+
+# Cross Validation Test
+
+import sys
+import yaml
+
+import torch
+import torch.nn as nn
+from torch.utils import data
+
+
+sys.path.append("..")
+from network.OCTDataSet import *
+from network.OCTOptimization import *
+from network.OCTTransform import *
+import time
+
+from LambdaSubnet import LambdaSubnet
+
+sys.path.append("../..")
+from utilities.FilesUtilities import *
+from utilities.TensorUtilities import *
+from framework.NetMgr import NetMgr
+from framework.ConfigReader import ConfigReader
+
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+
+sys.path.append("../dataPrepare_Tongren")
+from TongrenFileUtilities import *
+
+
+def printUsage(argv):
+    print("============ Cross Validation Test OCT MultiSurface Network =============")
+    print("Usage:")
+    print(argv[0], " yaml_Config_file_path")
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Error: input parameters error.")
+        printUsage(sys.argv)
+        return -1
+
+    # parse config file
+    configFile = sys.argv[1]
+    hps = ConfigReader(configFile)
+    print(f"Experiment: {hps.experimentName}")
+    assert "IVUS" not in hps.experimentName
+
+    if hps.dataIn1Parcel:
+        if -1==hps.k and 0==hps.K:  # do not use cross validation
+            testImagesPath = os.path.join(hps.dataDir, "test", f"images.npy")
+            testLabelsPath = os.path.join(hps.dataDir, "test", f"surfaces.npy") if hps.existGTLabel else None
+            testIDPath = os.path.join(hps.dataDir, "test", f"patientID.json")
+        else:  # use cross validation
+            testImagesPath = os.path.join(hps.dataDir,"test", f"images_CV{hps.k:d}.npy")
+            testLabelsPath = os.path.join(hps.dataDir,"test", f"surfaces_CV{hps.k:d}.npy")
+            testIDPath    = os.path.join(hps.dataDir,"test", f"patientID_CV{hps.k:d}.json")
+    else:
+        if -1==hps.k and 0==hps.K:  # do not use cross validation
+            testImagesPath = os.path.join(hps.dataDir, "test", f"patientList.txt")
+            testLabelsPath = None
+            testIDPath = None
+        else:
+            print(f"Current do not support Cross Validation and not dataIn1Parcel\n")
+            assert(False)
+
+    testData = OCTDataSet(testImagesPath, testIDPath, testLabelsPath,  transform=None, hps=hps)
+
+    # construct network
+    net = eval(hps.network)(hps=hps)
+    # Important:
+    # If you need to move a model to GPU via .cuda(), please do so before constructing optimizers for it.
+    # Parameters of a model after .cuda() will be different objects with those before the call.
+    net.to(device=hps.device)
+
+    # Load network
+    netMgr = NetMgr(net, hps.netPath, hps.device)
+    netMgr.loadNet("test")
+
+
+    # test
+    testStartTime = time.time()
+    net.eval()
+    with torch.no_grad():
+        testBatch = 0
+        net.setStatus("test")
+        net.m_epoch = net.m_runParametersDict['epoch']
+        for batchData in data.DataLoader(testData, batch_size=hps.batchSize, shuffle=False, num_workers=0):
+            testBatch += 1
+            # S is surface location in (B,S,W) dimension, the predicted Mu
+            batchLambda = net.forward(batchData['images'], gaussianGTs=batchData['gaussianGTs'], GTs = batchData['GTs'], layerGTs=batchData['layers'], riftGTs=batchData['riftWidth'])
+            Lambda = torch.cat((Lambda, batchLambda)) if testBatch != 1 else batchLambda
+
+        print(f"Lambda.shape = {Lambda.shape}")
+        print(f"mean of Lambda = {torch.mean(Lambda, dim=[0,2])}")
+        print(f"min of Lambda  = {torch.min(torch.min(Lambda, dim=0)[0], dim=-1)}")
+        print(f"max of Lambda  = {torch.max(torch.max(Lambda, dim=0)[0], dim=-1)}")
+
+
+    testEndTime = time.time()
+
+    print(f"============ End of Output Lambda: {hps.experimentName} ===========")
+
+
+if __name__ == "__main__":
+    main()
