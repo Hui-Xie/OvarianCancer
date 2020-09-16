7c1418a5edad0fae9fec860ff559f330e8f45523
Sheen <sheenxh@gmail.com>
2020-09-16 18:34:32 -0500

    add residual head loss

--- OvarianCancerSurvival/network/ResponseNet.py
+++ OvarianCancerSurvival/network/ResponseNet.py
@@ -1,5 +1,6 @@
 
 import torch.nn as nn
+import torch
 
 import sys
 sys.path.append("../..")
@@ -13,12 +14,16 @@ class ResponseNet(BasicModel):
         '''
         super().__init__()
         self.hps = hps
+        self.m_resdualClassWeight = torch.tenosr([1.0/item for item in hps.residudalClassPercent]).to(hps.device)
+        self.m_chemoClassWeight = torch.tenosr([1.0/item for item in hps.chemoClassPercent]).to(hps.device)
 
         self.m_mobilenet = MobileNetV3(hps.inputChannels)
         self.m_residualSizeHead = nn.Conv2d(1280, hps.widthResidualHead, kernel_size=1, stride=1, padding=0, bias=False)
         self.m_survivalHead = nn.Conv2d(1280, hps.widthSurvialHead, kernel_size=1, stride=1, padding=0, bias=False)
         self.m_chemoResponseHead = nn.Conv2d(1280, hps.widthChemoHead, kernel_size=1, stride=1, padding=0, bias=False)
         self.m_ageHead = nn.Conv2d(1280, hps.widthAgeHead, kernel_size=1, stride=1, padding=0, bias=False)
+
+
 
     def forward(self, inputs, GTs=None):
         # compute outputs
@@ -27,11 +32,25 @@ class ResponseNet(BasicModel):
         x = inputs
 
         x = self.m_mobilenet(x)
-        residualFeature = self.m_residualSizeHead(x)
+        residualFeature = self.m_residualSizeHead(x) # size: 1x4x1x1
         survivalFeature = self.m_survivalHead(x)
         chemoResponseFeature = self.m_chemoResponseHead(x)
         ageFeature = self.m_ageHead(x)
 
-        #todo: implement Head and its loss computation.
+        #todo: implement Head and its loss computation
+        #residual tumor size
+        residualFeature = residualFeature.view(1, self.hps.widthResidualHead)
+        residualGT = GTs['ResidualTumor']+1 # from [-1,0,1,2] to [0,1,2,3]
+        residualPredict = torch.argmax(residualFeature)-1 # from [0,1,2,3] to [-1,0,1,2]
+        residualCELossFunc = nn.CrossEntropyLoss(weight=self.m_resdualClassWeight)
+        residualLoss = residualCELossFunc(residualFeature, residualGT)
 
-        return None
+
+
+
+
+
+
+
+
+        return residualPredict, residualLoss
