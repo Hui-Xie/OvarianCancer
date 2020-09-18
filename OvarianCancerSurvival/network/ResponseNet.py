86b6089fd58dcb9099ea8cbd60e19672343d712a
Sheen <sheenxh@gmail.com>
2020-09-17 22:37:40 -0500

    debug survival time prediction

--- OvarianCancerSurvival/network/ResponseNet.py
+++ OvarianCancerSurvival/network/ResponseNet.py
@@ -40,7 +40,7 @@ class ResponseNet(BasicModel):
         #residual tumor size
         residualFeature = residualFeature.view(1, self.hps.widthResidualHead)
         residualPredict = torch.argmax(residualFeature)-1 # from [0,1,2,3] to [-1,0,1,2]
-        residualGT = torch.tensor(GTs['ResidualTumor'] + 1).to(device).view(1,1)   # from [-1,0,1,2] to [0,1,2,3]
+        residualGT = torch.tensor(GTs['ResidualTumor'] + 1).to(device)  # from [-1,0,1,2] to [0,1,2,3]
         residualCELossFunc = nn.CrossEntropyLoss(weight=self.m_residualClassWeight)
         residualLoss = residualCELossFunc(residualFeature, residualGT)
 
@@ -49,7 +49,7 @@ class ResponseNet(BasicModel):
         chemoPredict = torch.argmax(chemoFeature) # [0,1]
         chemoLoss = 0.0
         if GTs['ChemoResponse'] != -100: # -100 ignore index
-            chemoGT = torch.tensor(GTs['ChemoResponse']).to(device).view(1,1)  # [0,1]
+            chemoGT = torch.tensor(GTs['ChemoResponse']).to(device)  # [0,1]
             chemoCELossFunc = nn.CrossEntropyLoss(weight=self.m_chemoClassWeight)
             chemoLoss = chemoCELossFunc(chemoFeature, chemoGT)
 
@@ -58,15 +58,15 @@ class ResponseNet(BasicModel):
         agePredict = torch.argmax(ageFeature)  # range [0,100)
         ageLoss = 0.0
         if GTs['Age'] != -100:  # -100 ignore index
-            ageGT = torch.tensor(GTs['Age']).to(device).view(1,1)  # range [0,100)
+            ageGT = torch.tensor(GTs['Age']).to(device)  # range [0,100)
             ageCELossFunc = nn.CrossEntropyLoss()
             ageLoss = ageCELossFunc(ageFeature, ageGT)
 
 
         # survival time:
         survivalFeature = survivalFeature.view(self.hps.widthSurvialHead)
-        P = nn.functional.sigmoid(survivalFeature)  # survival prob
-        survivalPredict = torch.argmax(P>= 0.5)  # argmax return the  position of the last maximum.
+        P = torch.sigmoid(survivalFeature)  # survival prob
+        survivalPredict = torch.argmax((P>= 0.5).int())  # argmax return the  position of the last maximum.
         survivalLoss = 0.0
         if (GTs['SurvivalMonths'] != -100) and (GTs['Censor'] != -100):
             z = int(GTs['SurvivalMonths']+0.5)
@@ -76,7 +76,7 @@ class ResponseNet(BasicModel):
 
                 # normalize expz_i and P[i]
                 R = self.hps.widthSurvialHead- z # expRange
-                expz_i = -torch.floatTensor(list(range(0,R))).to(device)
+                expz_i = -torch.tensor(list(range(0,R)), dtype=torch.float32, device=device)
                 expz_i = torch.exp(expz_i)
                 expSum = expz_i.sum()
                 expz_i = expz_i/expSum
