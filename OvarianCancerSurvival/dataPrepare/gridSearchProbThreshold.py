7ab134111c047bdf7f89756e63dd216602575a2e
Sheen <sheenxh@gmail.com>
2020-09-29 14:05:19 -0500

    add nonlabel processing

--- OvarianCancerSurvival/dataPrepare/gridSearchProbThreshold.py
+++ OvarianCancerSurvival/dataPrepare/gridSearchProbThreshold.py
@@ -14,16 +14,20 @@ def main():
 
     epsilon =1e-8
 
-    print("ProbThreshold,\tACC,\tTPR,\tTNR,\tSum(ACC_TPR_TNR),")
-    for td in np.arange(0.001, 0.999, 0.002):  # td: threshlod
+    print("ProbThreshold,\tACC,\tTPR,\tTNR,\tSum(ACC_TPR_TNR),\tnIgnore")
+    for td in np.arange(0.001, 0.999, 0.002):  # td: threshold
         keys = list(probDict.keys())
         nTotal = len(keys)
         TP = 0
         FP = 0
         FN = 0
         TN = 0
+        nIgnore = 0
 
         for MRN in keys:
+            if probDict[MRN]['GT'] == -100:
+                nIgnore += 1
+                continue
             if probDict[MRN]['Prob1'] < td:
                 if  probDict[MRN]['GT'] == 0:
                     TN +=1
@@ -35,11 +39,11 @@ def main():
                 else:
                     TP +=1
 
-        assert nTotal == TP+TN+FP+FN
+        assert nTotal == TP+TN+FP+FN + nIgnore
         ACC = (TP+TN)*1.0/(TP+TN+FP+FN)
         TPR = TP*1.0/(TP+FN+epsilon)  # sensitivity
         TNR = TN*1.0/(TN+FP+epsilon)  # specificity
-        print(f"{td:.3f},\t{ACC:.2f},\t{TPR:.2f},\t{TNR:.2f},\t{ACC+TPR+TNR:.3f},")
+        print(f"{td:.3f},\t{ACC:.2f},\t{TPR:.2f},\t{TNR:.2f},\t{ACC+TPR+TNR:.3f},\t{nIgnore:02d},")
 
 
 if __name__ == "__main__":
