c8e8bb766f82d11adebe68cb4cb02568c37455cf
Sheen <sheenxh@gmail.com>
2020-10-10 21:46:58 -0500

    add lrPatience para in config file

--- OCT2SysDisease/network/OCT2SysD_Train.py
+++ OCT2SysDisease/network/OCT2SysD_Train.py
@@ -83,7 +83,7 @@ def main():
     # lrScheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=100, min_lr=1e-8, threshold=0.02, threshold_mode='rel')
 
     # math.log(0.5,0.98) = 34, this scheduler equals scale 0.5 per 100 epochs.
-    lrScheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.98, patience=3, min_lr=1e-8, threshold=0.02, threshold_mode='rel')
+    lrScheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.98, patience=hps.lrPatience, min_lr=1e-8, threshold=0.02, threshold_mode='rel')
     net.setLrScheduler(lrScheduler)
 
     # Load network
