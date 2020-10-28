d0cf5b47ae245690bb598e2070936f5c43d20145
Sheen <sheenxh@gmail.com>
2020-10-28 13:02:53 -0500

    prepare generate en-face images

---
+++ OCTMultiSurfaces/EnFaceProjection/generateEnfaceImages.py
@@ -0,0 +1,21 @@
+# generate en-face images from segmentation result and OCT volume
+
+imageIDPath = ""
+
+OCTVolumeDir = ""
+segXmlDir =""
+outputDir =""
+
+
+# read volumeID into list
+
+# read xml segmentation into array
+
+# read volume
+
+# define output emtpy array
+
+# fill the output array
+
+# output file
+
