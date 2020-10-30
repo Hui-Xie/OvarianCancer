82e5606f6fa47901e87cc34f578c29eea635e20d
Sheen <sheenxh@gmail.com>
2020-10-30 14:24:08 -0500

    add volume by volume segmentation output for BES_3K

---
+++ OCTMultiSurfaces/network/CVTestSurfaces_BES3K_volume.py
@@ -0,0 +1,134 @@
+
+# Test BES_3K volume by volume and output its latent vector.
+
+import sys
+import yaml
+
+import torch
+import torch.nn as nn
+from torch.utils import data
+
+
+sys.path.append(".")
+from OCTDataSetVolume import OCTDataSetVolume
+from SurfacesUnet import SurfacesUnet
+from OCTOptimization import *
+from OCTTransform import *
+import time
+
+sys.path.append("../..")
+from utilities.FilesUtilities import *
+from utilities.TensorUtilities import *
+from framework.NetMgr import NetMgr
+from framework.ConfigReader import ConfigReader
+
+import numpy as np
+import datetime
+
+sys.path.append("../dataPrepare_Tongren")
+from TongrenFileUtilities import saveNumpy2OCTExplorerXML
+
+
+def printUsage(argv):
+    print("============ Test OCT BES_3K MultiSurface Network with input volume by volume =============")
+    print("Usage:")
+    print(argv[0], " yaml_Config_file_path")
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Error: input parameters error.")
+        printUsage(sys.argv)
+        return -1
+
+    # output config
+    MarkGTDisorder = False
+    MarkPredictDisorder = False
+
+    outputXmlSegFiles = True
+
+
+    OutputNumImages = 0
+    # choose from 0, 1,2,3:----------
+    # 0: no image output; 1: Prediction; 2: GT and Prediction; 3: Raw, GT, Prediction
+    # 4: Raw, GT, Prediction with GT superpose in one image
+    comparisonSurfaceIndex = None
+    #comparisonSurfaceIndex = 2 # compare the surface 2 (index starts from  0)
+    # GT uses red, while prediction uses green
+
+    needLegend = True
+
+    # parse config file
+    configFile = sys.argv[1]
+    hps = ConfigReader(configFile)
+    print(f"Experiment: {hps.experimentName}")
+    assert "IVUS" not in hps.experimentName
+
+    if hps.numSurfaces == 9:
+        surfaceNames = ['ILM', 'RNFL-GCL', 'IPL-INL', 'INL-OPL', 'OPL-HFL', 'BMEIS', 'IS/OSJ', 'IB_RPE', 'OB_RPE']
+    if hps.numSurfaces == 10:
+        surfaceNames = ['ILM', 'RNFL-GCL', 'GCL-IPL', 'IPL-INL', 'INL-OPL', 'OPL-HFL', 'BMEIS', 'IS/OSJ', 'IB_RPE', 'OB_RPE']
+
+    # construct network
+    net = eval(hps.network)(hps=hps)
+    # Important:
+    # If you need to move a model to GPU via .cuda(), please do so before constructing optimizers for it.
+    # Parameters of a model after .cuda() will be different objects with those before the call.
+    net.to(device=hps.device)
+
+    # Load network
+
+    netMgr = NetMgr(net, hps.netPath, hps.device)
+    netMgr.loadNet("test")
+
+    testImagesPath = hps.dataDir
+    testLabelsPath = None
+    testIDPath    = None
+
+    testData = OCTDataSetVolume(testImagesPath, testIDPath, testLabelsPath,  transform=None, hps=hps)
+
+    # test
+    testStartTime = time.time()
+    net.eval()
+    with torch.no_grad():
+        testBatch = 0
+        for batchData in data.DataLoader(testData, batch_size=hps.batchSize, shuffle=False, num_workers=0):
+            testBatch += 1
+
+            # squeeze the extra dimension of data with volume input
+            inputs = batchData['images'].squeeze(dim=0)
+            volumeID = batchData['IDs'][0]  # erase tuple wrapper
+
+            # S is surface location in (B,S,W) dimension, the predicted Mu
+            S, _loss = net.forward(inputs, gaussianGTs=None, GTs = None, layerGTs=None)
+
+            # Error Std and mean
+            predicition = S
+            if hps.groundTruthInteger:
+                predicition = (S + 0.5).int()  # as ground truth are integer, make the output also integers.
+
+            predicition = predicition.cpu().numpy()
+
+            outputXMLFilename = hps.xmlOutputDir2 + f"/{volumeID}_Sequence_Surfaces_Prediction.xml"
+            if outputXmlSegFiles and (not os.path.exists(outputXMLFilename)):
+                saveNumpy2OCTExplorerXML(volumeID, predicition, surfaceNames, hps.xmlOutputDir2, hps.refXMLFile)
+
+
+    testEndTime = time.time()
+
+    # final output result:
+    curTime = datetime.datetime.now()
+    timeStr = f"{curTime.year}{curTime.month:02d}{curTime.day:02d}_{curTime.hour:02d}{curTime.minute:02d}{curTime.second:02d}"
+
+    with open(os.path.join(hps.outputDir,f"output_Volume_{timeStr}.txt"), "w") as file:
+        hps.printTo(file)
+        file.write("\n=======net running parameters=========\n")
+        file.write(f"Test time: {testEndTime-testStartTime} seconds.\n")
+        file.write(f"net.m_runParametersDict:\n")
+        [file.write(f"\t{key}:{value}\n") for key, value in net.m_runParametersDict.items()]
+
+    print(f"============ End of BES3K test for OCT Multisurface Network: {hps.experimentName} ===========")
+
+
+if __name__ == "__main__":
+    main()
---
+++ OCTMultiSurfaces/network/OCTDataSetVolume.py
@@ -0,0 +1,240 @@
+from torch.utils import data
+import numpy as np
+import json
+import os
+import torch
+import torchvision.transforms as TF
+import glob
+
+from network.OCTAugmentation import *
+
+# input volume one by one.
+
+class OCTDataSetVolume(data.Dataset):
+    def __init__(self, imagesPath, IDPath=None, labelPath=None, transform=None, hps=None):
+        super().__init__()
+        self.hps = hps
+
+        self.m_imagesList = None
+        self.m_labels = None
+        self.m_IDs = None
+
+        if imagesPath is not None:
+            self.m_imagesList = glob.glob(imagesPath + f"/*_Volume.npy")
+            self.m_imagesList.sort()
+
+        if labelPath is not None:
+            self.m_labels = torch.from_numpy(np.load(labelPath).astype(np.float32)).to(self.hps.device, dtype=torch.float)  # slice, num_surface, W
+
+        if IDPath is not None:
+            with open(IDPath) as f:
+                self.m_IDs = json.load(f)
+        self.m_transform = transform
+
+
+    def __len__(self):
+        return len(self.m_imagesList)
+
+    def generateGradientImage(self, image, gradChannels):
+        '''
+        gradient should use both-side gradient approximation formula: (f_{i+1}-f_{i-1})/2,
+        and at boundaries of images, use single-side gradient approximation formula: (f_{i}- f_{i-1})/2
+        :param image: in size: HxW
+        :param gradChannels:  integer
+        :return:
+        '''
+        e = 1e-8
+        H,W =image.shape
+        device = image.device
+
+        if self.hps.bothSideGrad:
+            image_1H = image[0:-2, :]  # size: H-2,W
+            image1H = image[2:, :]
+            gradH = torch.cat(((image[1,:]- image[0,:]).view(1,W),
+                               (image1H - image_1H)/2.0,
+                               (image[-1,:]-image[-2,:]).view(1,W)), dim=0)  # size: H,W; grad90
+
+            image_1W = image[:, 0:-2]  # size: H,W-2
+            image1W = image[:, 2:]
+            gradW = torch.cat(((image[:,1]- image[:,0]).view(H,1),
+                               (image1W - image_1W)/2,
+                               (image[:,-1]- image[:,-2]).view(H,1)), dim=1)  # size: H,W; grad0
+
+            gradMagnitudeHW = torch.sqrt(torch.pow(gradH, 2) + torch.pow(gradW, 2))
+
+            # normalize
+            min = torch.min(gradMagnitudeHW)
+            max = torch.max(gradMagnitudeHW)
+            gradMagnitudeHW = (gradMagnitudeHW - min) / (max - min + e)  # in range [0,1]
+
+            std, mean = torch.std_mean(gradH)
+            gradH = (gradH - mean) / (std + e)  # in range[-1,1]
+
+            std, mean = torch.std_mean(gradW)
+            gradW = (gradW - mean) / (std + e)
+
+            if gradChannels >= 3:
+                onesImage = torch.ones_like(image)
+                negOnesImage = -onesImage
+                signHW = torch.where(gradH * gradW >= 0, onesImage, negOnesImage)
+                gradDirectionHW = torch.atan(signHW * torch.abs(gradH) / (torch.abs(gradW) + e))
+                std, mean = torch.std_mean(gradDirectionHW)
+                gradDirectionHW = (gradDirectionHW - mean) / (std + e)
+
+            if gradChannels >= 5:
+                image45_0 = image[0:-2, 2:]  # size: H-2,W-2
+                image45_1 = image[2:, 0:-2]  # size: H-2,W-2
+                grad45 = torch.cat(((image[1:-1,0]-image[0:-2,1]).view(H-2,1),
+                                    (image45_1 - image45_0)/2.0,
+                                    (image[1:-1,-2]-image[0:-2,-1]).view(H-2,1)), dim=1) # size: H-2, W
+
+                row0 = torch.cat((torch.tensor([0.0],device=device).view(1,1), (image[1,0:-1]-image[0,1:]).view(1,W-1)),dim=1)  # size: 1,W
+                row_1 = torch.cat(((image[-1,0:-1]-image[-2,1:]).view(1,W-1), torch.tensor([0.0],device=device).view(1,1)),dim=1) # size:1,W
+                grad45 = torch.cat((row0,
+                                    grad45,
+                                    row_1), dim=0)
+                std, mean = torch.std_mean(grad45)
+                grad45 = (grad45 - mean) / (std + e)
+
+                image135_0 = image[0:-2, 0:-2]  # size: H-2,W-2
+                image135_1 = image[2:, 2:]  # size: H-2,W-2
+                grad135 = torch.cat(((image[2:, 1] - image[1:-1, 0]).view(H - 2, 1),
+                                    (image135_1 - image135_0) / 2.0,
+                                    (image[1:-1, -1] - image[0:-2, -2]).view(H - 2, 1)), dim=1)  # size: H-2, W
+
+                row0 = torch.cat(
+                    ((image[1, 1:] - image[0, 0:-1]).view(1, W - 1), torch.tensor([0.0], device=device).view(1, 1)),
+                    dim=1)  # size: 1,W
+                row_1 = torch.cat(
+                    (torch.tensor([0.0], device=device).view(1, 1), (image[-1, 1:] - image[-2, 0:-1]).view(1, W - 1)),
+                    dim=1)  # size:1,W
+                grad135 = torch.cat((row0,
+                                    grad135,
+                                    row_1), dim=0)
+                std, mean = torch.std_mean(grad135)
+                grad135 = (grad135 - mean) / (std + e)
+
+            if gradChannels >= 7:
+                sign135_45 = torch.where(grad135 * grad45 >= 0, onesImage, negOnesImage)
+                gradDirection135_45 = torch.atan(sign135_45 * torch.abs(grad135) / (torch.abs(grad45) + e))
+                std, mean = torch.std_mean(gradDirection135_45)
+                gradDirection135_45 = (gradDirection135_45 - mean) / (std + e)
+
+        else: # use single Side Grad:
+            image0H = image[0:-1,:]  # size: H-1,W
+            image1H = image[1:,  :]
+            gradH   = image1H-image0H
+            gradH = torch.cat((gradH, torch.zeros((1, W), device=device)), dim=0)  # size: H,W; grad90
+
+            image0W = image[:,0:-1]  # size: H,W-1
+            image1W = image[:,1:  ]
+            gradW = image1W - image0W
+            gradW = torch.cat((gradW, torch.zeros((H, 1), device=device)), dim=1)  # size: H,W; grad0
+
+            gradMagnitudeHW = torch.sqrt(torch.pow(gradH,2)+torch.pow(gradW,2))
+
+            # normalize
+            min = torch.min(gradMagnitudeHW)
+            max = torch.max(gradMagnitudeHW)
+            gradMagnitudeHW = (gradMagnitudeHW- min)/(max-min+e)   # in range [0,1]
+
+            std, mean = torch.std_mean(gradH)
+            gradH = (gradH - mean) / (std + e)  # in range[-1,1]
+
+            std, mean = torch.std_mean(gradW)
+            gradW = (gradW - mean) / (std + e)
+
+            if gradChannels>=3:
+                onesImage = torch.ones_like(image)
+                negOnesImage = -onesImage
+                signHW = torch.where(gradH * gradW >= 0, onesImage, negOnesImage)
+                gradDirectionHW = torch.atan(signHW * torch.abs(gradH) / (torch.abs(gradW) + e))
+                std, mean = torch.std_mean(gradDirectionHW)
+                gradDirectionHW = (gradDirectionHW - mean) / (std + e)
+
+            if gradChannels >= 5:
+                image45_0 = image[0:-1,1:]  # size: H-1,W-1
+                image45_1 = image[1:,0:-1]  # size: H-1,W-1
+                grad45 = image45_1 - image45_0 # size: H-1,W-1
+                grad45 = torch.cat((torch.zeros((H-1,1), device=device), grad45), dim=1)
+                grad45 = torch.cat((grad45, torch.zeros((1, W), device=device)), dim=0)
+                std, mean = torch.std_mean(grad45)
+                grad45 = (grad45 - mean) / (std + e)
+
+                image135_0 = image[0:-1, 0:-1]  # size: H-1,W-1
+                image135_1 = image[1:, 1:]  # size: H-1,W-1
+                grad135 = image135_1 - image135_0  # size: H-1,W-1
+                grad135 = torch.cat((grad135, torch.zeros((H - 1, 1), device=device)), dim=1)
+                grad135 = torch.cat((grad135, torch.zeros((1, W), device=device)), dim=0)
+                std, mean = torch.std_mean(grad135)
+                grad135 = (grad135 - mean) / (std + e)
+
+            if gradChannels >= 7:
+                sign135_45 = torch.where(grad135 * grad45 >= 0, onesImage, negOnesImage)
+                gradDirection135_45 = torch.atan(sign135_45 * torch.abs(grad135) / (torch.abs(grad45) + e))
+                std, mean = torch.std_mean(gradDirection135_45)
+                gradDirection135_45 = (gradDirection135_45 - mean) / (std + e)
+
+        # since July 23th, 2020, gradMagnitudeHW are put into the final channel
+        if 1 == gradChannels:
+            return gradMagnitudeHW
+        elif 2 == gradChannels:
+            return gradH, gradW,
+        elif 3 == gradChannels:
+             return gradH, gradW, gradMagnitudeHW
+        elif 4 == gradChannels:
+            return gradH, gradW, gradDirectionHW, gradMagnitudeHW
+        elif 5 == gradChannels:
+            return gradH, gradW, grad45, grad135, gradMagnitudeHW
+        elif 6 == gradChannels:
+            return gradH, gradW, grad45, grad135, gradDirectionHW, gradMagnitudeHW
+        elif 7 == gradChannels:
+            return gradH, gradW, grad45, grad135, gradDirectionHW, gradDirection135_45, gradMagnitudeHW
+        else:
+            print(f"Currently do not support gradChannels >7")
+            assert False
+            return None
+
+    def __getitem__(self, index):
+        volumePath = self.m_imagesList[index]
+        data = torch.from_numpy(np.load(volumePath).astype(np.float32)).to(self.hps.device, dtype=torch.float)
+        B,H,W = data.shape
+
+        basename = os.path.basename(volumePath)
+        volumeName, ext = os.path.splitext(basename)
+        imageID = volumeName
+
+        label = None
+
+        newData = torch.empty((B, self.hps.inputChannels, H,W), dtype=torch.float, device=self.hps.device)
+
+        for b in range(B):
+            image = data[b,]
+            if 0 != self.hps.gradChannels:
+                grads = self.generateGradientImage(image, self.hps.gradChannels)
+                image = image.unsqueeze(dim=0)
+                for grad in grads:
+                    image = torch.cat((image, grad.unsqueeze(dim=0)),dim=0)
+            newData[b,] = image
+
+        data = newData
+
+        layerGT = []
+        riftWidthGT = []
+
+        result = {"images": data,
+                  "GTs": [] if label is None else label,
+                  "gaussianGTs": [],
+                  "IDs": imageID,
+                  "layers": layerGT,
+                  "riftWidth": riftWidthGT}
+        return result
+
+
+
+
+
+
+
+
+
