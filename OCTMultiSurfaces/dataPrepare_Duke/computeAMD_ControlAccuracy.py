7eadc102cbcfdbaf80f3078a3e462608ca78120b
Sheen <sheenxh@gmail.com>
2020-11-17 16:28:24 -0600

    add AMD and Control separately computation

---
+++ OCTMultiSurfaces/dataPrepare_Duke/computeAMD_ControlAccuracy.py
@@ -0,0 +1,62 @@
+# compute AMD and control group accuracy separately
+
+predictDir ="/home/hxie1/data/OCT_Duke/numpy_slices/log/SoftSepar3Unet/expDuke_20201113A_FixLambda2Unet/testResult/xml"
+gtDir = "/home/hxie1/data/OCT_Duke/numpy_slices/test"
+
+N = 3
+W = 361
+B = 51  #Bscan number for each volume
+hPixelSize =  3.24
+
+import glob
+import numpy as np
+import os
+import torch
+import sys
+sys.path.append("..")
+from dataPrepare_Tongren.TongrenFileUtilities import getSurfacesArray
+from network.OCTOptimization import computeErrorStdMuOverPatientDimMean
+
+
+AMDXmlList = glob.glob(predictDir + f"/AMD_*_images_Sequence_Surfaces_Prediction.xml")
+ControlXmlList = glob.glob(predictDir + f"/Control_*_images_Sequence_Surfaces_Prediction.xml")
+
+twoGroupDict = {"AMD":AMDXmlList, "Control":ControlXmlList}
+for groupName, xmlList in twoGroupDict.items():
+    # for AMD
+    Num = len(xmlList)
+    predictAll = np.empty([Num*B,N,W])
+    gtAll = np.empty([Num*B,N, W])
+    i = 0
+    for xmlPath in xmlList:
+        volumeSurfaces = getSurfacesArray(xmlPath)
+        predictAll[i:i+B,] = volumeSurfaces
+        _, stemname = os.path.split(xmlPath)
+        volumeName = stemname[0: stemname.find("_images_Sequence_Surfaces_Prediction.xml")]
+        gtSliceList = glob.glob(gtDir +f"/{volumeName}_surfaces_s[0-5][0-9].npy")
+        assert len(gtSliceList) == B
+        gtSliceList.sort()
+        for slicePath in gtSliceList:
+            bscanSurface = np.load(slicePath)
+            gtAll[i,] = bscanSurface
+            i = i+1
+
+    assert predictAll.shape == gtAll.shape
+    predictAll = torch.from_numpy(predictAll)
+    gtAll = torch.from_numpy(gtAll)
+    stdSurfaceError, muSurfaceError, stdError, muError = computeErrorStdMuOverPatientDimMean(predictAll, gtAll,
+                                                          slicesPerPatient=B,  hPixelSize=hPixelSize, goodBScansInGtOrder=None)
+    print("GroupName: {groupName}")
+    print(f"case number = {Num}")
+    print(f"stdSurfaceError = {stdSurfaceError}")
+    print(f"muSurfaceError = {muSurfaceError}")
+    print(f"stdError = {stdError}")
+    print(f"muError = {muError}")
+    print(f"===============\n")
+
+
+
+
+
+
+
