637c0adc9e7de906a92b33cee2462222f9716c1b
Sheen <sheenxh@gmail.com>
2020-11-25 15:47:51 -0600

    debug

--- Test/testCfgReader.py
+++ Test/testCfgReader.py
@@ -7,7 +7,7 @@ import os
 import torch
 
 
-cfgFilePath = "/home/sheen/projects/DeepLearningSeg/OCTMultiSurfaces/SoftSepar3Unet/testConfig_Duke/expDuke_20201117A_SurfaceSubnet_NoReLU.yaml"
+cfgFilePath = "/home/hxie1/Projects/DeepLearningSeg/OCTMultiSurfaces/SoftSepar3Unet/testConfig_Duke/expDuke_20201117A_SurfaceSubnet_NoReLU.yaml"
 
 hps = ConfigReader(cfgFilePath)
 
