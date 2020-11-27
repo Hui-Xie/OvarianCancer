fbcb1b78a787ac0836886551833a271a7227e623
Sheen <sheenxh@gmail.com>
2020-11-27 17:34:20 -0600

    update

--- OCTMultiSurfaces/SoftSepar3Unet/gridSearchLambda_UnconstrainedOpt_RiftGT.py
+++ OCTMultiSurfaces/SoftSepar3Unet/gridSearchLambda_UnconstrainedOpt_RiftGT.py
@@ -25,8 +25,8 @@ hPixelSize = 3.24
 rSource = "GTR" # "predictR"  # "GTR": use R ground truth
 
 # Lambda search range:
-lambda0_min, lambda0_max, lambda0_step = 0, 0.00004, 0.0000002
-lambda1_min, lambda1_max, lambda1_step = 0, 0.00004, 0.0000002
+lambda0_min, lambda0_max, lambda0_step = 0, 0.0000004, 0.000000002
+lambda1_min, lambda1_max, lambda1_step = 0, 0.0000004, 0.000000002
 
 lambda0List = list(np.arange(lambda0_min, lambda0_max, lambda0_step))
 lambda1List = list(np.arange(lambda1_min, lambda1_max, lambda1_step))
