43ecc24a534c9ef283a740a61b576aa8739dbbbc
Sheen <sheenxh@gmail.com>
2020-12-17 18:56:34 -0600

    debug

--- OCT2SysDisease/network/OCT2SysD_Train.py
+++ OCT2SysDisease/network/OCT2SysD_Train.py
@@ -20,6 +20,8 @@ from ThicknessMap2Gender_A import ThicknessMap2Gender_A
 from ThicknessMap2HyperTensionNet_VGG16 import ThicknessMap2HyperTensionNet_VGG16
 from ThicknessMap2HyperTensionNet_HalfUNet import ThicknessMap2HyperTensionNet_HalfUNet
 from ThicknessMap2HyperTensionNet_VGG16BatchNorm import ThicknessMap2HyperTensionNet_VGG16BatchNorm
+form ThicknessMap2HyperTensionNet_E import ThicknessMap2HyperTensionNet_E
+
 from OCT2SysD_Tools import *
 
 sys.path.append("../..")
