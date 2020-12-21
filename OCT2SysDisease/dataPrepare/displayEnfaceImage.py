87f27176919378125ebd4276b02e34964a01915b
Sheen <sheenxh@gmail.com>
2020-12-21 17:56:43 -0600

    add 31x25 display enface images

---
+++ OCT2SysDisease/dataPrepare/displayEnfaceImage.py
@@ -0,0 +1,53 @@
+# visualize en-face image of 31x25 size
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+outputDir = "/home/hxie1/data/temp"
+
+def main():
+    if len(sys.argv) != 2:
+        print("Error: input parameters error.")
+        print(f"{sys.argv[0]}  fullPathOfEnfaceVolume")
+        return -1
+    enfaceVolumePath = sys.argv[1]
+
+    enfaceVolume = np.load(enfaceVolumePath)
+    N,H,W = enfaceVolume.shape
+    print(f"enface volume: N={N}, H={H}, W={W}")
+
+    basename = os.path.basename(enfaceVolumePath)
+    volumename, ext = os.path.splitext(basename)
+
+    assert N == 9
+    #surfaceNames = ['ILM', 'RNFL-GCL', 'GCL-IPL', 'IPL-INL', 'INL-OPL', 'OPL-HFL', 'BMEIS', 'IS/OSJ', 'IB_RPE', 'OB_RPE']  # 10 surfaces.
+    layerNames = ['RNFL', 'GCL', 'IPL', 'INL', 'OPL', 'ONL','ELM','PR', 'RPE']
+
+
+    f = plt.figure(frameon=False)
+    DPI = f.dpi
+    rowSubplot = 3  # for 31x25 enface images
+    colSubplot = 3
+    f.set_size_inches(W * colSubplot / float(DPI), H * rowSubplot / float(DPI))
+
+    plt.margins(0)
+    plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)  # very important for erasing unnecessary margins.
+
+    textLocx = int(W*0.01)
+    textLocy = int(H*0.3)
+
+    for i in range(N):
+        subploti = plt.subplot(rowSubplot, colSubplot, i+1)
+        subploti.imshow(enfaceVolume[i,], cmap='gray')
+        subploti.axis('off')
+        subploti.text(textLocx, textLocy, layerNames[i], fontsize=8)
+
+    outputFilePath = os.path.join(outputDir, volumename+ ".png")
+    plt.savefig(outputFilePath, dpi='figure', bbox_inches='tight', pad_inches=0)
+    plt.close()
+    print(f"image file output at {outputFilePath}")
+
+if __name__ == "__main__":
+    main()
