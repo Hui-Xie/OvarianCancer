68f9c72a13e09ccc7ddf3cd50e573dbf71fe048d
Sheen <sheenxh@gmail.com>
2020-12-25 16:12:59 -0600

    debug

--- framework/ConfigReader.py
+++ framework/ConfigReader.py
@@ -17,7 +17,7 @@ class ConfigReader(object):
             cfg = yaml.load(file, Loader=yaml.FullLoader)
         self.__dict__.update(cfg)
 
-        self.device = eval(self.device) if "device" not in self.__dict__ else None
+        self.device = eval(self.device) if "device" in self.__dict__ else None
 
         if "IVUS" not in self.dataDir:
             self.rotation = False
