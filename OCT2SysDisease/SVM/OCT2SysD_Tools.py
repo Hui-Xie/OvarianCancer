c7e369e06c751fae4e7c9111c13875e260a0b918
Sheen <sheenxh@gmail.com>
2020-12-25 13:41:18 -0600

    finish numpy retrieve data basic part

---
+++ OCT2SysDisease/SVM/OCT2SysD_Tools.py
@@ -0,0 +1,39 @@
+
+import csv
+
+def readBESClinicalCsv(gtPath):
+    '''
+        ID,Eye,gender,Age$,VA$,Pres_VA$,VA_Corr$,IOP$,Ref_Equa$,AxialLength$,Axiallength_26_ormore_exclude$,Glaucoma_exclude$,Retina_exclude$,Height$,Weight$,Waist_Circum$,Hip_Circum$,BP_Sys$,BP_Dia$,hypertension_bp_plus_history$,Diabetes$final,Dyslipidemia_lab$,Dyslipidemia_lab_plus_his$,Hyperlipdemia_treat$_WithCompleteZero,Pulse$,Cognitive$,Depression_Correct_wyx,Drink_quanti_includ0$,SmokePackYears$,Glucose$_Corrected2015,CRPL$_Corrected2015,Choles$_Corrected2015,HDL$_Corrected2015,LDL$_Correcetd2015,TG$_Corrected2015
+2.0,1,2,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+3.0,1,1,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+5.0,1,1,53,0.6,0.6,1,12,-1,25.14,,,,186,108,118,110,135,70,1,0,0,0,0,75,30,24.44,0.5,1,5.15,7.2,5.63,0.99,4.47,1.36
+6.0,1,2,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+7.0,1,2,53,0.8,0.8,1,18,0.75,21.57,,,,164,72,102,109,143,82,1,0,1,1,1,58,22,31.11,0,0,5.25,0.79,5.8,1.24,4.49,1.9
+8.0,1,1,57,0.6,0.6,0.8,18,-0.38,23.84,,,,165,80,105,107,132,65,1,1,0,0,0,93,23,23.33,1,5.25,14.37,0.01,4.29,1.18,3.07,1.56
+9.0,1,2,60,0.6,0.6,1,15,0.38,22.48,,,,156,74,103,116,140,86,0,0,0,0,0,69,27,23.33,0,2,5.04,0.28,4.97,1.65,3.26,1.09
+10.0,1,2,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+13.0,1,1,60,0.5,0.5,0.8,11,-1.38,23.14,,,,167.5,57,80,91,104,69,0,0,0,0,0,72,30,20,2,4.5,4.19,0.21,3.22,1.93,1.48,0.42
+
+
+    '''
+    gtDict = {}
+    with open(gtPath, newline='') as csvfile:
+        csvList = list(csv.reader(csvfile, delimiter=',', quotechar='|'))
+        tableHead = csvList[0]
+        nCols = len(tableHead)
+        csvList = csvList[1:]  # erase table head
+        for row in csvList:
+            if len(row[0])>0:
+                ID = int(float(row[0]))
+                gtDict[ID] = {}
+                for c in range(1, nCols):
+                    key = tableHead[c]
+                    if 0 != len(row[c]):
+                        if '.' in row[c]:
+                            value = float(row[c])
+                        else:
+                            value = int(row[c])
+                    else:
+                        value = -100
+                    gtDict[ID][key] = value
+    return gtDict
--- OCT2SysDisease/SVM/thickness2HyTension_SVM.py
+++ OCT2SysDisease/SVM/thickness2HyTension_SVM.py
@@ -1,14 +1,119 @@
 # Use SVM method to predict hypertension from thickness map
 
 from sklearn import svm
+import glob
 import sys
+import os
+import fnmatch
+import numpy as np
+import yaml
 sys.path.append("../..")
 from framework.ConfigReader import ConfigReader
+sys.path.append(".")
+from OCT2SysD_Tools import readBESClinicalCsv
 
 def printUsage(argv):
     print("============ OCT Thickness to Hypertension Using SVM =============")
     print("Usage:")
     print(argv[0], " yaml_Config_file_full_path")
+
+def retrieveImageData_label(mode, hps):
+    '''
+
+    :param mode: "training", "validation", or "test"
+    :param hps:
+    :return:
+    '''
+    if mode == "training":
+        IDPath = hps.trainingDataPath
+    elif mode == "validation":
+        IDPath = hps.validationDataPath
+    elif mode == "test":
+        IDPath = hps.testDataPath
+    else:
+        print(f"OCT2SysDiseaseDataSet mode error")
+        assert False
+
+    with open(IDPath, 'r') as idFile:
+        IDList = idFile.readlines()
+    IDList = [item[0:-1] for item in IDList]  # erase '\n'
+
+    # get all correct volume numpy path
+    allVolumesList = glob.glob(hps.dataDir + f"/*{hps.volumeSuffix}")
+    nonexistIDList = []
+
+    # make sure volume ID and volume path has strict corresponding order
+    volumePaths = []  # number of volumes is about 2 times of IDList
+    IDsCorrespondVolumes = []
+
+    volumePathsFile = os.path.join(hps.dataDir, mode + "_VolumePaths.txt")
+    IDsCorrespondVolumesPathFile = os.path.join(hps.dataDir, mode + "_IDsCorrespondVolumes.txt")
+
+    # save related file in order to speed up.
+    if os.path.isfile(volumePathsFile) and os.path.isfile(IDsCorrespondVolumesPathFile):
+        with open(volumePathsFile, 'r') as file:
+            lines = file.readlines()
+        volumePaths = [item[0:-1] for item in lines]  # erase '\n'
+
+        with open(IDsCorrespondVolumesPathFile, 'r') as file:
+            lines = file.readlines()
+        IDsCorrespondVolumes = [item[0:-1] for item in lines]  # erase '\n'
+
+    else:
+        for i, ID in enumerate(IDList):
+            resultList = fnmatch.filter(allVolumesList, "*/" + ID + f"_O[D,S]_*{hps.volumeSuffix}")
+            resultList.sort()
+            numVolumes = len(resultList)
+            if 0 == numVolumes:
+                nonexistIDList.append(ID)
+            else:
+                volumePaths += resultList
+                IDsCorrespondVolumes += [ID, ] * numVolumes
+
+        if len(nonexistIDList) > 0:
+            print(f"Error: nonexistIDList:\n {nonexistIDList}")
+            assert False
+
+        # save files
+        with open(volumePathsFile, "w") as file:
+            for v in volumePaths:
+                file.write(f"{v}\n")
+        with open(IDsCorrespondVolumesPathFile, "w") as file:
+            for v in IDsCorrespondVolumes:
+                file.write(f"{v}\n")
+
+    NVolumes = len(volumePaths)
+    assert (NVolumes == len(IDsCorrespondVolumes))
+
+    # load all volumes into memory
+    volumes = np.empty((NVolumes, hps.inputChannels, hps.imageH, hps.imageW), dtype=np.float)
+    for i, volumePath in enumerate(volumePaths):
+        oneVolume = np.load(volumePath).astype(np.float)
+        volumes[i,] = oneVolume
+
+    # normalize training volumes and save mean and std for using in validation and test data
+    epsilon = 1.0e-8
+    normalizationYamlPath = os.path.join(hps.netPath, hps.trainNormalizationStdMeanYamlName)
+    if mode == "training":
+        std = np.std(volumes)
+        mean = np.mean(volumes)
+        volumes = (volumes - mean) / (std + epsilon)  # size: NxCxHxW
+        with open(normalizationYamlPath, 'w') as outfile:
+            d = {"std": std.item(), "mean": mean.item()}
+            yaml.dump(d, outfile, default_flow_style=False)
+
+    elif (mode == "validation") or (mode == "test"):
+        with open(normalizationYamlPath, 'r') as infile:
+            cfg = yaml.load(infile, Loader=yaml.FullLoader)
+        volumes = (volumes - cfg["mean"]) / (cfg["std"] + epsilon)  # size: NxCxHxW
+    else:
+        print(f"OCT2SysDiseaseDataSet mode error")
+        assert False
+
+    # todo: we need return flat volumes and corresponding label
+
+    return volumes
+
 
 def main():
     if len(sys.argv) != 2:
@@ -20,6 +125,16 @@ def main():
     configFile = sys.argv[1]
     hps = ConfigReader(configFile)
     print(f"Experiment: {hps.experimentName}")
+
+    # load training data, validation, and test data
+    fullLabels = readBESClinicalCsv(hps.GTPath)
+
+
+    # train SVM
+
+
+    # test and print out
+
 
 
     # load
