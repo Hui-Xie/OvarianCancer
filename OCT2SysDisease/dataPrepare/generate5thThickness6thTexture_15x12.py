d6601bd83800af61c1eab608404ff9b586f080ee
Sheen <sheenxh@gmail.com>
2020-12-31 10:52:07 -0600

    add generate 15x12 5thThickness and 6thTexture map

--- OCT2SysDisease/dataPrepare/generate5thThickness6thTexture_15x12.py
+++ OCT2SysDisease/dataPrepare/generate5thThickness6thTexture_15x12.py
@@ -1,8 +1,8 @@
 # generate 5th thickness as channel 0 and 6th texture as channel 1
 
-outputDir= "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/5thThickness_6thTexture_2x31x25"
-thicknessDir = "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/thicknessEnfaceMap_9x31x25"
-textureDir = "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/textureEnfaceMap_9x31x25"
+outputDir= "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/5thThickness_6thTexture_2x15x12"
+thicknessDir = "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/thicknessEnfaceMap_9x15x12"
+textureDir = "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/textureEnfaceMap_9x15x12"
 
 
 import glob
@@ -19,6 +19,8 @@ thicknessVolumeList.sort()
 nVolumes = len(thicknessVolumeList)
 print(f"total {nVolumes} volumes")
 
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)  # recursive dir creation
 
 for thickessPath in thicknessVolumeList:
     texturePath = thickessPath.replace("thickness", "texture")
@@ -27,13 +29,13 @@ for thickessPath in thicknessVolumeList:
     outputFilename = outputFilename.replace("_thickness_", "_5thThickness_6thTexture_")
     outputPath = os.path.join(outputDir, outputFilename)
 
-    newVolume = np.empty((2,31,25), dtype=np.float)
+    newVolume = np.empty((2,15,12), dtype=np.float)
     # read volume
     thicknessVolume = np.load(thickessPath)  # BxHxW
-    assert (9,31,25) == thicknessVolume.shape
+    assert (9,15,12) == thicknessVolume.shape
 
     textureVolume = np.load(texturePath)
-    assert (9, 31, 25) == textureVolume.shape
+    assert (9,15, 12) == textureVolume.shape
 
     newVolume[0,] = thicknessVolume[c1,]
     newVolume[1,] = textureVolume[c2,]
