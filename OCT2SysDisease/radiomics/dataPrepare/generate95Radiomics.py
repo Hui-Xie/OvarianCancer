0d416a0e5e711604d0cf2d950f3ee103f3c81533
Sheen <sheenxh@gmail.com>
2021-02-13 18:36:06 -0600

    use nrrd texture and mask as input to pyradiomics

--- OCT2SysDisease/radiomics/dataPrepare/generate95Radiomics.py
+++ OCT2SysDisease/radiomics/dataPrepare/generate95Radiomics.py
@@ -18,27 +18,29 @@ from radiomics import featureextractor
 
 def main():
     # Get the PyRadiomics logger (default log-level = INFO
-    logger = radiomics.logger
-    logger.setLevel(logging.DEBUG)  # set level to DEBUG to include debug log messages in log file
+    # logger = radiomics.logger
+    # logger.setLevel(logging.DEBUG)  # set level to DEBUG to include debug log messages in log file
 
     # Write out all log entries to a file
-    handler = logging.FileHandler(filename=os.path.join(outputRadiomicsDir, 'generateLog_radiomics.txt'), mode='w')
-    formatter = logging.Formatter("%(levelname)s:%(name)s: %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    # handler = logging.FileHandler(filename=os.path.join(outputRadiomicsDir, 'generateLog_radiomics.txt'), mode='w')
+    # formatter = logging.Formatter("%(levelname)s:%(name)s: %(message)s")
+    # handler.setFormatter(formatter)
+    # logger.addHandler(handler)
 
-    textureList = glob.glob(textureDir + f"/*_Volume_s15_texture.tif")
+    textureList = glob.glob(textureDir + f"/*_Volume_s15_texture.nrrd")
     print(f"total {len(textureList)} texture files.")
     for texturePath in textureList:
         sliceName = os.path.basename(texturePath)  # 334027_OS_17817_Volume_s15_texture.tif
-        maskName = sliceName.replace("_texture.tif", "_mask.tif") # 334027_OS_17817_Volume_s15_mask.tif
+        maskName = sliceName.replace("_texture.nrrd", "_mask.nrrd") # 334027_OS_17817_Volume_s15_mask.tif
         maskPath = os.path.join(maskDir, maskName)
 
-        radiomicsArrayName =sliceName.replace("_texture.tif", f"_{K}radiomics.npy")
+        radiomicsArrayName =sliceName.replace("_texture.nrrd", f"_{K}radiomics.npy")
         radiomicsArrayPath = os.path.join(outputRadiomicsDir, radiomicsArrayName)
 
         # Initialize feature extractor using the settings file
         extractor = featureextractor.RadiomicsFeatureExtractor(radiomicsCfgPath)
+        extractor.settings["force2D"] = True
+        extractor.settings["force2Ddimension"] = 0
         featureVector = extractor.execute(texturePath, maskPath, label=1)
 
         nFeatures = 0
