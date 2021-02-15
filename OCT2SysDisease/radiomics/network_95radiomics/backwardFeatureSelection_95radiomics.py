eadadc661bc9c17b312194d5b39881c22d5a145a
Sheen <sheenxh@gmail.com>
2021-02-15 10:11:33 -0600

    modify feature selection for 95 radiomics

--- OCT2SysDisease/radiomics/network_95radiomics/backwardFeatureSelection_95radiomics.py
+++ OCT2SysDisease/radiomics/network_95radiomics/backwardFeatureSelection_95radiomics.py
@@ -12,7 +12,7 @@ clinicalGTPath = "/home/hxie1/data/BES_3K/GTs/BESClinicalGT_Analysis.csv"
 
 outputDir = "/home/hxie1/data/BES_3K/GTs/95radiomics_ODOS_10CV"
 
-imageSize =  [1,95]
+# input 95 radiomics size: [1,95]
 numRadiomics = 95
 keyName = "hypertension_bp_plus_history$"
 srcSuffix = "_Volume_s15_95radiomics.npy"
@@ -57,14 +57,13 @@ def retrieveImageData_label(mode):
     # get all correct volume numpy path
     allVolumesList = glob.glob(dataDir + f"/*{srcSuffix}")
     nonexistIDList = []
-    multipleImages_IDList = []
 
     # make sure volume ID and volume path has strict corresponding order
     volumePaths = []  # number of volumes is about 2 times of IDList
     IDsCorrespondVolumes = []
 
-    volumePathsFile = os.path.join(hps.dataDir, mode + f"_{hps.ODOS}_VolumePaths.txt")
-    IDsCorrespondVolumesPathFile = os.path.join(hps.dataDir, mode + f"_{hps.ODOS}_IDsCorrespondVolumes.txt")
+    volumePathsFile = os.path.join(dataDir, mode + f"_{ODOS}_FeatureSelection_VolumePaths.txt")
+    IDsCorrespondVolumesPathFile = os.path.join(dataDir, mode + f"_{ODOS}_FeatureSelection_IDsCorrespondVolumes.txt")
 
     # save related file in order to speed up.
     if os.path.isfile(volumePathsFile) and os.path.isfile(IDsCorrespondVolumesPathFile):
@@ -78,21 +77,17 @@ def retrieveImageData_label(mode):
 
     else:
         for i, ID in enumerate(IDList):
-            resultList = fnmatch.filter(allVolumesList, "*/" + ID + f"_{hps.ODOS}_*{hps.volumeSuffix}")  # only for OD or OS data
+            resultList = fnmatch.filter(allVolumesList, "*/" + ID + f"_O[D,S]_*{srcSuffix}")  # for OD or OS data
             resultList.sort()
             numVolumes = len(resultList)
             if 0 == numVolumes:
                 nonexistIDList.append(ID)
-            elif numVolumes > 1:
-                multipleImages_IDList.append(ID)
             else:
                 volumePaths += resultList
-                IDsCorrespondVolumes += [ID, ] # one ID, one volume
+                IDsCorrespondVolumes += [ID, ]*numVolumes # multiple IDs
 
         if len(nonexistIDList) > 0:
-            print(f"nonExistIDList of {hps.ODOS} in {mode}:\n {nonexistIDList}")
-        if len(multipleImages_IDList) > 0:
-            print(f"List of ID corresponding multiple {hps.ODOS} images in {mode}:\n {multipleImages_IDList}")
+            print(f"nonExistIDList of {ODOS} in {mode}:\n {nonexistIDList}")
 
         # save files
         with open(volumePathsFile, "w") as file:
@@ -106,51 +101,25 @@ def retrieveImageData_label(mode):
     assert (NVolumes == len(IDsCorrespondVolumes))
 
     # load all volumes into memory
-    assert hps.imageW == 1
-    volumes = np.empty((NVolumes, hps.inputChannels, hps.imageH), dtype=np.float) # size:NxCxH for 9x9 sector array
+    volumes = np.empty((NVolumes, numRadiomics), dtype=np.float) # size:NxnRadiomics
     for i, volumePath in enumerate(volumePaths):
         oneVolume = np.load(volumePath).astype(np.float)
-        volumes[i,:] = oneVolume
-    volumes = volumes.reshape(-1,hps.inputChannels*hps.imageH* hps.imageW) # size: Nx(CxHxW)
+        volumes[i,:] = oneVolume[0,:]  # as oneVolume has a size [1, nRadiomics]
 
-    fullLabels = readBESClinicalCsv(hps.GTPath)
+    fullLabels = readBESClinicalCsv(clinicalGTPath)
 
-    labelTable = np.empty((NVolumes, 22), dtype=np.float) #  size: Nx22
-    # labelTable head: patientID,                                          (0)
-    #             "hypertension_bp_plus_history$", "gender", "Age$",'IOP$', 'AxialLength$', 'Height$', 'Weight$', 'Waist_Circum$', 'Hip_Circum$', 'SmokePackYears$',
-    # columnIndex:         1                           2        3       4          5             6          7             8              9                10
-    #              'Pulse$', 'Drink_quanti_includ0$', 'Glucose$_Corrected2015', 'CRPL$_Corrected2015',  'Choles$_Corrected2015', 'HDL$_Corrected2015', 'LDL$_Correcetd2015',
-    # columnIndex:   11            12                           13                      14                       15                       16                  17
-    #              'TG$_Corrected2015',  BMI,   WaistHipRate,  LDL/HDL
-    # columnIndex:      18                 19       20         21
+    # get HBP labels
+    labelTable = np.empty((NVolumes, 2), dtype=np.float) #  size: Nx2 with (ID, Hypertension)
     for i in range(NVolumes):
         id = int(IDsCorrespondVolumes[i])
-        labelTable[i,0] = id
+        labelTable[i, 0] = id
+        labelTable[i, 1] = fullLabels[id][keyName]
 
-        #appKeys: ["hypertension_bp_plus_history$", "gender", "Age$", 'IOP$', 'AxialLength$', 'Height$', 'Weight$',
-        #          'Waist_Circum$', 'Hip_Circum$', 'SmokePackYears$', 'Pulse$', 'Drink_quanti_includ0$', 'Glucose$_Corrected2015', 'CRPL$_Corrected2015',
-        #          'Choles$_Corrected2015', 'HDL$_Corrected2015', 'LDL$_Correcetd2015', 'TG$_Corrected2015']
-        for j,key in enumerate(hps.appKeys):
-            oneLabel = fullLabels[id][key]
-            if "gender" == key:
-                oneLabel = oneLabel - 1
-            labelTable[i, 1+j] = oneLabel
-
-        # compute BMI, WaistHipRate, LDL/HDL
-        if labelTable[i,7]==-100 or labelTable[i,6] ==-100:
-            labelTable[i, 19] = -100 # emtpty value
-        else:
-            labelTable[i, 19] = labelTable[i,7]/ ((labelTable[i,6]/100.0)**2)  # weight is in kg, height is in cm.
-
-        if labelTable[i, 8] == -100 or labelTable[i, 9] == -100:
-            labelTable[i, 20] = -100
-        else:
-            labelTable[i, 20] = labelTable[i,8]/ labelTable[i,9]  # both are in cm.
-
-        if labelTable[i, 17] == -100 or labelTable[i, 16] == -100:
-            labelTable[i, 21] = -100
-        else:
-            labelTable[i, 21] = labelTable[i, 17] / labelTable[i, 16]  # LDL/HDL, bigger means more risk to hypertension.
+    # save log information:
+    print(f"{mode} dataset feature selection: NVolumes={NVolumes}\n")
+    rate1 = labelTable[:,1].sum() * 1.0 / NVolumes
+    rate0 = 1 - rate1
+    print(f"{mode} dataset feature selection: proportion of 0,1 = [{rate0},{rate1}]\n")
 
     return volumes, labelTable
 
