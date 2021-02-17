ff1678ba393486067785d9b2eecd1477628f7968
Sheen <sheenxh@gmail.com>
2021-02-17 16:08:44 -0600

    debug

--- OCT2SysDisease/radiomics/network_3D_INL_RPE/generate3D100Radiomics.py
+++ OCT2SysDisease/radiomics/network_3D_INL_RPE/generate3D100Radiomics.py
@@ -9,6 +9,7 @@ K = 100   # the number of extracted features.
 
 fastFold = 3
 # for example, a=[0,1,2,3,4,5,6,7,8,9] with N=10
+#  0: original order:  [0,1,2,3,4,5,6,7,8,9]
 #  1: reverse   [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 #  2: middlefold  [5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
 #  3: middle_fold_reverse: b = a[0:N//2]; b.reverse(); b+a[N//2:]  => [4, 3, 2, 1, 0, 5, 6, 7, 8, 9]
