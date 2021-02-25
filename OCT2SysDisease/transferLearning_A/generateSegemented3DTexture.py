7c0bb22d893589c61add2037310c3056fc9e9874
Sheen <sheenxh@gmail.com>
2021-02-25 09:41:08 -0600

    finished output of normalized segmented 3D texture

--- OCT2SysDisease/transferLearning_A/generateSegemented3DTexture.py
+++ OCT2SysDisease/transferLearning_A/generateSegemented3DTexture.py
@@ -12,6 +12,29 @@
 #  E.  normalization: (image-mean)/std.
 #  F.  save as npy file.
 
+
+'''
+segmented images mean and std, which is already normalized in the output of output:
+/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/volume3D_s0tos9_indexSpace/volumes
+
+(base) [c-xwu000:volume3D_s0tos9_indexSpace]#cat outputLog_20210224_174528.txt
+total 6499 xml files.
+output segmented and normalized retina regions from surface 0 to surface9.
+Now all masks have been generated in /home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/volume3D_s0tos9_indexSpace/masks
+meanBscans = [0.05199834 0.05284249 0.0532695  0.0537375  0.05438357 0.0549899
+ 0.0558392  0.05696388 0.05825874 0.0598245  0.06093201 0.06143699
+ 0.06085205 0.05910843 0.05703062 0.05591611 0.05711094 0.05924026
+ 0.06057586 0.06111015 0.06090984 0.0599876  0.05905826 0.05801464
+ 0.05705912 0.056168   0.05544387 0.05496442 0.05464729 0.05429021
+ 0.0540758 ]
+stdBscans = [0.14800051 0.14917299 0.14980883 0.1504818  0.15139771 0.15180002
+ 0.15233731 0.15314984 0.15386566 0.15500712 0.15508947 0.15453244
+ 0.15276628 0.15002041 0.14744838 0.14581831 0.14734901 0.14996752
+ 0.15186637 0.15354847 0.15457117 0.15440928 0.15420679 0.15358165
+ 0.15280802 0.15198185 0.15122363 0.1508052  0.15057934 0.15009072
+ 0.14986088]
+'''
+
 import os
 
 outputDir = "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/volume3D_s0tos9_indexSpace"
