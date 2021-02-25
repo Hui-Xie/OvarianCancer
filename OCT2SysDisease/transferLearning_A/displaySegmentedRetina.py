6cb45d80dc77f878ab0364d2eccc55e8763a1068
Sheen <sheenxh@gmail.com>
2021-02-25 10:09:12 -0600

    display normalized segmented retina

---
+++ OCT2SysDisease/transferLearning_A/displaySegmentedRetina.py
@@ -0,0 +1,24 @@
+
+srcDir="/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/volume3D_s0tos9_indexSpace/volumes"
+srcFile = "359_OD_483_Volume_SegTexture.npy"
+outputDir = "/home/hxie1/temp"
+
+k = 15
+
+from PIL import Image  # for Tiff image save
+import numpy as np
+import os
+
+volumePath = os.path.jion(srcDir, srcFile)
+volume = np.load(volumePath)
+S, H,W = volume.shape
+
+sliceName = srcFile[0:srcFile.find("_SegTexture.npy")] + f"_s{k}.tif"
+outputPath = os.path.join(outputDir, sliceName)
+
+print(f"volume.shape = {volume.shape}")
+
+slice = volume[k]
+
+# use PIL to save tiff image, which keep single byte for each pixel.
+Image.fromarray(slice).save(outputPath)
