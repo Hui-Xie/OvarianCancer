02c2fcb0a2e7c3b87395bac3cfe138a75e99e542
Sheen <sheenxh@gmail.com>
2021-03-02 14:36:56 -0600

    add 10 fold division

--- OCT2SysDisease/combineThickness_Radiomics_Clinical/logisticRegrression_ThicknessRadiomicsClinical_indexSpace_10CV.py
+++ OCT2SysDisease/combineThickness_Radiomics_Clinical/logisticRegrression_ThicknessRadiomicsClinical_indexSpace_10CV.py
@@ -7,7 +7,7 @@ input: 100 3D radiomics feature in index space:
        clinical data:
            10 clinical features ['Age', 'IOP', 'AxialLength', 'Pulse', 'Drink', 'Glucose', 'Triglyceride', 'BMI', 'WaistHipRate', 'LDLoverHDL']
            "/home/hxie1/data/BES_3K/GTs/BESClinicalGT_Analysis.csv"
-       in above data, radiomics data is the first choose data: use their IDs to further choose thickness and clinical data.
+
 
 10 fold crossing validation experiment:
 output result in csv format:
@@ -15,7 +15,7 @@ CV   #TrainningSamples #ValidationSamples #TestSamples #TrainingAccuracy #Valida
 
 Algorithm:
 1  use /home/hxie1/data/BES_3K/log/logisticReg_thicknessRadiomicsClinical_10CV/allIDwith10ClinicalFtrs.txt which filtered out non-existing clinical data ID
-   and deleted myopia, glaucoma, macula disease.
+   and deleted myopia, glaucoma, macula diseases.
 2  read al clinical data into a fullTable, get all ID's HBP Label;
 3  according to HBP label, divide 1895 IDs into 10 folds.
 4  for 10 CV test:
@@ -29,6 +29,7 @@ radiomicsDir = "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3
 thicknessDir = "/home/hxie1/data/BES_3K/W512NumpyVolumes/log/SurfacesNet/expBES3K_20201126A_genXml/testResult/thickness9Sector_9x9"
 clinicalGTPath = "/home/hxie1/data/BES_3K/GTs/BESClinicalGT_Analysis.csv"
 IDPath = "/home/hxie1/data/BES_3K/log/logisticReg_thicknessRadiomicsClinical_10CV/allIDwith10ClinicalFtrs.txt"
+K = 10 # 10-fold Cross validation
 
 outputDir = "/home/hxie1/data/BES_3K/log/logisticReg_thicknessRadiomicsClinical_10CV"
 
@@ -85,11 +86,96 @@ def main():
     allIDList = [item[0:-1] for item in allIDList]  # erase '\n'
 
     # 2  read al clinical data into a fullTable, get all ID's HBP Label;
+    # read clinical GT, and extract hypertension with history tag
+    fullLabels = readBESClinicalCsv(clinicalGTPath)
+    nHBP0 = 0  # statistics hypertension
+    nHBP1 = 0
+    for ID in allIDList:
+        if fullLabels[ID][keyName] == 1:
+            nHBP1 += 1
+        elif fullLabels[ID][keyName] == 0:
+            nHBP0 += 1
+        else:
+            continue
+    nHBP01 = nHBP0 + nHBP1
+    print(f"in IDFile: {IDPath}: {len(allIDList)} patients, taggedHBP0 = {nHBP0}, taggedHBP1 = {nHBP1}, total {nHBP01} patients.")
+
+    ID_HBP_Array = np.zeros((nHBP01, 2), dtype=np.uint32)  # ID and HBP
+    i = 0
+    for ID in allIDList:
+        tag = fullLabels[ID][keyName]
+        if 1 == tag or 0 == tag:
+            ID_HBP_Array[i, 0] = int(ID)
+            ID_HBP_Array[i, 1] = int(tag)
+            i += 1
 
     # 3  according to HBP label, divide 1895 IDs into 10 folds.
+    # K-Fold division
+    patientID_0 = ID_HBP_Array[np.nonzero(ID_HBP_Array[:, 1] == 0), 0]  # size: 1xnHBP0
+    patientID_1 = ID_HBP_Array[np.nonzero(ID_HBP_Array[:, 1] == 1), 0]
+    patientID_0 = list(set(list(patientID_0[0, :])))  # erase repeated IDs
+    patientID_1 = list(set(list(patientID_1[0, :])))
+    assert len(patientID_0) == nHBP0
+    assert len(patientID_1) == nHBP1
+    print(f"After erasing repeated ID: Num_response0 = {len(patientID_0)}, Num_response1= {len(patientID_1)}, total={len(patientID_0) + len(patientID_1)}")
 
+    # split files in sublist, this is a better method than before.
+    patientID0SubList = []
+    step = len(patientID_0) // K
+    for i in range(0, K * step, step):
+        nexti = i + step
+        patientID0SubList.append(patientID_0[i:nexti])
+    for i in range(K * step, len(patientID_0)):
+        patientID0SubList[i - K * step].append(patientID_0[i])
 
+    patientID1SubList = []
+    step = len(patientID_1) // K
+    for i in range(0, K * step, step):
+        nexti = i + step
+        patientID1SubList.append(patientID_1[i:nexti])
+    for i in range(K * step, len(patientID_1)):
+        patientID1SubList[i - K * step].append(patientID_1[i])
 
+    patientsSubList = []
+    for i in range(K):
+        patientsSubList.append(patientID0SubList[i] + patientID1SubList[K - 1 - i])  # help each fold has same number of samples
+
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)  # recursive dir creation
+
+    outputValidation = True
+    print("")
+    for k in range(0, K):
+        partitions = {}
+        partitions["test"] = patientsSubList[k]
+
+        if outputValidation:
+            k1 = (k + 1) % K  # validation k
+            partitions["validation"] = patientsSubList[k1]
+        else:
+            k1 = k
+            partitions["validation"] = []
+
+        partitions["training"] = []
+        for i in range(K):
+            if i != k and i != k1:
+                partitions["training"] += patientsSubList[i]
+
+        # save to file
+        with open(os.path.join(outputDir, f"testID_10Clinical_NoMGMDiseases_{K}CV_{k}.csv"), "w") as file:
+            for v in partitions["test"]:
+                file.write(f"{int(v)}\n")
+
+        if outputValidation:
+            with open(os.path.join(outputDir, f"validationID_10Clinical_NoMGMDiseases_{K}CV_{k}.csv"), "w") as file:
+                for v in partitions["validation"]:
+                    file.write(f"{int(v)}\n")
+
+        with open(os.path.join(outputDir, f"trainID_10Clinical_NoMGMDiseases_{K}CV_{k}.csv"), "w") as file:
+            for v in partitions["training"]:
+                file.write(f"{int(v)}\n")
+
+        print(f"CV: {k}/{K}: test: {len(partitions['test'])} patients;  validation: {len(partitions['validation'])} patients;  training: {len(partitions['training'])} patients;")
 
     # 4  for 10 CV test:
     #    4.1 Assemble clinical features, thickness features, and radiomics features.
