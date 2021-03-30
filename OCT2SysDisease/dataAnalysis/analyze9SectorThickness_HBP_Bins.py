e6b65e53a0cd7f6640921f52e622b4dd98dab5b0
Sheen <sheenxh@gmail.com>
2021-03-30 15:58:49 -0500

    debug

--- OCT2SysDisease/dataAnalysis/analyze9SectorThickness_HBP_Bins.py
+++ OCT2SysDisease/dataAnalysis/analyze9SectorThickness_HBP_Bins.py
@@ -252,14 +252,14 @@ def main():
 
                 # use fixed circle with text
                 if x0BinSize[i] != 0:
-                    yLoc = 0 if i%2==0 else 0.1
-                    circle0 = plt.Circle((binCenter[i], 0), radius=0.1, facecolor=None, alpha=0.5, edgecolor=pltColors[i], fill=False)
+                    yLoc = 0 if i%2==0 else 0.05
+                    circle0 = plt.Circle((binCenter[i], 0), radius=0.05, facecolor=None, alpha=0.5, edgecolor=pltColors[i], fill=False)
                     plt.text(binCenter[i]-binGap/4,yLoc, f"{x0BinSize[i]:d}",fontsize=6)
                     ax.add_patch(circle0)
 
                 if x1BinSize[i] != 0:
-                    yLoc = 0.9 if i % 2 == 0 else 1
-                    circle1 = plt.Circle((binCenter[i], 1), radius=0.1, facecolor=None, alpha=0.5, edgecolor=pltColors[i], fill=False)
+                    yLoc = 0.95 if i % 2 == 0 else 1
+                    circle1 = plt.Circle((binCenter[i], 1), radius=0.05, facecolor=None, alpha=0.5, edgecolor=pltColors[i], fill=False)
                     plt.text(binCenter[i] - binGap / 4, yLoc, f"{x1BinSize[i]:d}", fontsize=6)
                     ax.add_patch(circle1)
 
