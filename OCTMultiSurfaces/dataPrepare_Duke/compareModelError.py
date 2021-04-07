eb46822c7f8e5e47468904c8ecc55e9672ead62e
Sheen <sheenxh@gmail.com>
2021-04-07 09:46:52 -0500

    prepare compare different model errors

---
+++ OCTMultiSurfaces/dataPrepare_Duke/compareModelError.py
@@ -0,0 +1,79 @@
+# Compare model error with student t-test and MSE.
+# model 1: pure surface branch
+# model 2: SoftSeparation model 2 with gradient paitwise term.
+
+
+predictDir1 = "/localscratch/Users/hxie1/data/OCT_Duke/numpy_slices/log/SurfaceSubnet_M/expDuke_20210322_SurfaceSubnet_M_iibi007/testResult/xml"
+predictDir2 = "/localscratch/Users/hxie1/data/OCT_Duke/numpy_slices/log/SoftSeparationNet_A/expDuke_SoftSeparation_A_20210329_iibi007/testResult/xml"
+gtDir = "/localscratch/Users/hxie1/data/OCT_Duke/numpy_slices/test"
+
+N = 3
+W = 361
+B = 51  #Bscan number for each volume
+hPixelSize =  3.24
+
+import glob
+import numpy as np
+import os
+import torch
+import sys
+sys.path.append("..")
+from dataPrepare_Tongren.TongrenFileUtilities import getSurfacesArray
+from network.OCTOptimization import computeErrorStdMuOverPatientDimMean
+sys.path.append("../..")
+from framework.NetTools import  columnHausdorffDist
+
+
+AMDXmlList = glob.glob(predictDir + f"/AMD_*_images_Sequence_Surfaces_Prediction.xml")
+ControlXmlList = glob.glob(predictDir + f"/Control_*_images_Sequence_Surfaces_Prediction.xml")
+
+print("Compute performance in AMD and Control group separately")
+print(f"predictDir= {predictDir}")
+print(f"gtDir = {gtDir}")
+print("===============")
+
+
+twoGroupDict = {"AMD":AMDXmlList, "Control":ControlXmlList}
+for groupName, xmlList in twoGroupDict.items():
+    Num = len(xmlList)
+    predictAll = np.empty([Num*B,N,W])
+    gtAll = np.empty([Num*B,N, W])
+    i = 0
+    for xmlPath in xmlList:
+        volumeSurfaces = getSurfacesArray(xmlPath)
+        predictAll[i:i+B,] = volumeSurfaces
+        _, stemname = os.path.split(xmlPath)
+        volumeName = stemname[0: stemname.find("_images_Sequence_Surfaces_Prediction.xml")]
+        gtSliceList = glob.glob(gtDir +f"/{volumeName}_surfaces_s[0-5][0-9].npy")
+        assert len(gtSliceList) == B
+        gtSliceList.sort()
+        for slicePath in gtSliceList:
+            bscanSurface = np.load(slicePath)
+            gtAll[i,] = bscanSurface
+            i = i+1
+
+    assert predictAll.shape == gtAll.shape
+    # use numpy for computing Hausdorff distance
+    columnHausdorffD = columnHausdorffDist(predictAll, gtAll).reshape((1, N))
+    # use torch.tensor for segmenation accuracy
+    predictAll = torch.from_numpy(predictAll)
+    gtAll = torch.from_numpy(gtAll)
+    stdSurfaceError, muSurfaceError, stdError, muError = computeErrorStdMuOverPatientDimMean(predictAll, gtAll,
+                                                          slicesPerPatient=B,  hPixelSize=hPixelSize, goodBScansInGtOrder=None)
+
+    print(f"GroupName: {groupName}")
+    print(f"case number = {Num}")
+    print(f"stdSurfaceError = {stdSurfaceError}")
+    print(f"muSurfaceError = {muSurfaceError}")
+    print(f"HausdorffDistance in pixel = {columnHausdorffD}")
+    print(f"HausdorffDistance in physical size (micrometer) = {columnHausdorffD*hPixelSize}")
+    print(f"stdError = {stdError}")
+    print(f"muError = {muError}")
+    print(f"===============")
+
+
+
+
+
+
+
