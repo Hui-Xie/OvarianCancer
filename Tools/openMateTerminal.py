05fd5cf2ab2ebf6eade7c57713ee505c49ebdd9f
Sheen <sheenxh@gmail.com>
2021-04-10 12:23:26 -0500

    update

---
+++ Tools/openMateTerminal.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("mate-terminal --tab -t \"LocalCode\" --tab -t \"Papers\" --tab -t \"learning\" --tab -t \"temp\" "
+          "--tab -t \"mountGPU\" --tab -t \"GPUCode1\" --tab -t \"GPULog\" --tab -t \"GPUNet\"   "
+          "--tab -t \"GPUCode2\" --tab -t \"GPUData\" --tab -t \"GPUtemp\" --tab -t \"python3\"   ")
