1bbe794798684b61e07af9c3153ac6ea2a88bb26
Hui Xie <sheenxh@gmail.com>
2021-04-23 12:00:42 -0500

    updated

--- Test/testPlt.py
+++ Test/testPlt.py
@@ -10,7 +10,7 @@ f.set_size_inches(W / float(DPI), H / float(DPI))
 plt.margins(0)
 plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0,hspace=0)  # very important for erasing unnecessary margins.
 
-image = np.zeros(H,W)
+image = np.zeros((H,W))
 for h in range(H):
     for w in range(W):
         if (h-H/2)**2 + (w-W/2)**2 == 10000:
@@ -20,4 +20,5 @@ for h in range(H):
 plt.imshow(image, cmap='gray')
 plt.axis('off')
 
+print("Output a circle image at currrent directory.")
 plt.savefig("./testPlt.png", dpi='figure', bbox_inches='tight', pad_inches=0)
