f96dd0746188f70ddf50bf91fc9debba5f676eea
Hui Xie <sheenxh@gmail.com>
2021-04-23 12:02:47 -0500

    update

--- Test/testPlt.py
+++ Test/testPlt.py
@@ -14,11 +14,11 @@ image = np.zeros((H,W))
 for h in range(H):
     for w in range(W):
         if (h-H/2)**2 + (w-W/2)**2 == 10000:
-            image[h,w] = 1
+            image[h,w] = 255
 
 
 plt.imshow(image, cmap='gray')
 plt.axis('off')
 
-print("Output a circle image at currrent directory.")
+print("Output a circle image at the currrent directory with name: testPlt.png")
 plt.savefig("./testPlt.png", dpi='figure', bbox_inches='tight', pad_inches=0)
