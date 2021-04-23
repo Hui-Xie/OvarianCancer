7bf5742d05cfdb5e6e79427c34ca2b606853ba6b
Hui Xie <sheenxh@gmail.com>
2021-04-23 11:57:02 -0500

    debug

--- Test/testPlt.py
+++ Test/testPlt.py
@@ -10,9 +10,14 @@ f.set_size_inches(W / float(DPI), H / float(DPI))
 plt.margins(0)
 plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0,hspace=0)  # very important for erasing unnecessary margins.
 
-image = np.random.rand(H,W)
-subplot1 = plt.subplot(1, 1, 1)
-subplot1.imshow(image, cmap='gray')
-subplot1.axis('off')
+image = np.zeros(H,W)
+for h in range(H):
+    for w in range(W):
+        if (h-H/2)**2 + (w-W/2)**2 == 10000:
+            image[h,w] = 1
+
+
+plt.imshow(image, cmap='gray')
+plt.axis('off')
 
 plt.savefig("./testPlt.png", dpi='figure', bbox_inches='tight', pad_inches=0)
