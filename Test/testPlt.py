faafd9bb2bff8fd9956bd1d50bfe3c68685dde70
Hui Xie <sheenxh@gmail.com>
2021-04-23 12:36:37 -0500

    debug

--- Test/testPlt.py
+++ Test/testPlt.py
@@ -5,15 +5,26 @@ f = plt.figure(frameon=False)
 DPI = f.dpi
 W = 512
 H = 496
-f.set_size_inches(W / float(DPI), H / float(DPI))
+subplotRow = 1
+subplotCol = 2
+f.set_size_inches(W*subplotCol / float(DPI), H*subplotRow / float(DPI))
 
 plt.margins(0)
 plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0,hspace=0)  # very important for erasing unnecessary margins.
 
-image = np.random.rand(H,W)
+image0 = np.random.rand(H,W)
+image1 = (1.0-image0)**2
 
-plt.imshow(image, cmap='gray', )
-plt.axis('off')
+subplot1 = plt.subplot(subplotRow, subplotCol, 1)
+subplot1.imshow(image0, cmap='gray', )
+subplot1.axis('off')
 
-print("Output a random image at the current directory with name: testPlt.png")
+subplot2 = plt.subplot(subplotRow, subplotCol, 2)
+subplot2.imshow(image1, cmap='gray', )
+subplot2.plot(range(0, W), np.arange(0,W), 'tab:red', linewidth=2)
+subplot2.legend("skewLine", loc='lower center')
+subplot2.axis('off')
+
+print("Output a random image with 2 subplots at the current directory with name: testPlt.png")
 plt.savefig("./testPlt.png", dpi='figure', bbox_inches='tight', pad_inches=0)
+plt.close()
