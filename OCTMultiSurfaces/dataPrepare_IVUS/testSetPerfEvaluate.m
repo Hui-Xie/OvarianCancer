6a5ea100755cbd724327d4d440d133bc28eff77c
Hui Xie <sheenxh@gmail.com>
2021-05-18 16:37:04 -0500

    update matlab function

--- OCTMultiSurfaces/dataPrepare_IVUS/testSetPerfEvaluate.m
+++ OCTMultiSurfaces/dataPrepare_IVUS/testSetPerfEvaluate.m
@@ -13,56 +13,76 @@ IIRC, in both cases matlab should exit once it is done.
 
 clear all
 clc
+predDirList=[
+'/raid001/users/hxie1/data/IVUS/polarNumpy_10percent/log/SurfaceSubnet_Q/expIVUS_20210518_SurfaceSubnetQ64_10percent_C_skm2/testResult/text',
+'/raid001/users/hxie1/data/IVUS/polarNumpy_10percent/log/SurfacesUnet_YufanHe_2/expIVUS_20210518_YufanHe_10percent_C_skm2/testResult/text',
 
-% predDir = '/raid001/users/hxie1/data/IVUS/polarNumpy/log/SurfaceSubnet_Q/expIVUS_20210514_SurfaceSubnetQ64_100percent_A_skm2/testResult/text';
-% predDir = '/raid001/users/hxie1/data/IVUS/polarNumpy/log/SurfacesUnet_YufanHe_2/expIVUS_20210514_YufanHe_100percent_A_skm2/testResult/text'
-% predDir = '/raid001/users/hxie1/data/IVUS/polarNumpy/log/SurfacesUnet/expUnetIVUS_Sigma0_20200302/realtime_testResult/text';
-% predDir = "/raid001/users/hxie1/data/IVUS/polarNumpy_10percent/log/SurfaceSubnet_Q/expIVUS_20210514_SurfaceSubnetQ64_10percent_A_skm2/testResult/text"
-predDir = "/raid001/users/hxie1/data/IVUS/polarNumpy_10percent/log/SurfacesUnet_YufanHe_2/expIVUS_20210514_YufanHe_10percent_A_skm2/testResult/text"
 
-%gtDir = '/raid001/users/hxie1/data/IVUS/Test_Set/Data_set_B/LABELS_obs1';
-gtDir = '/raid001/users/hxie1/data/IVUS/Test_Set/Data_set_B/LABELS_obs2_v1'; %Leixin used. It is best.
-%gtDir = '/raid001/users/hxie1/data/IVUS/Test_Set/Data_set_B/LABELS_obs2_v2';
-predictLumenFiles = dir(fullfile(predDir,'lum_*_003.txt'));
-predictLumenFiles = {predictLumenFiles.name}';
-N = numel(predictLumenFiles);
-fprintf("Prediction result dir: %s\n", predDir)
-fprintf("Test set has %d images comparing with ground truth at %s.\n", N, gtDir)
+]
+NPred = length(predDirList);
 
-% result data tables
-lumenDataTable = zeros(N, 4);
-mediaDataTable = zeros(N, 4);
-% column: jacc, dice, hd, pad
-% row: different file
-
-for i=1:N
-    predLumenFile = fullfile(predDir, predictLumenFiles{i});
-    gtLumenFile = fullfile(gtDir, predictLumenFiles{i});
-    prediction = dlmread(predLumenFile, ',')';
-    gt = dlmread(gtLumenFile, ',')';
-    [lumenDataTable(i,1), lumenDataTable(i,2), lumenDataTable(i,3), lumenDataTable(i,4)]= computePerformance_cart(prediction, gt, 384);
-    
-    predMediaFile = strrep(predLumenFile,'lum_','med_');
-    gtMediaFile = strrep(gtLumenFile,'lum_','med_');
-    prediction = dlmread(predMediaFile, ',')';
-    gt = dlmread(gtMediaFile, ',')';
-    [mediaDataTable(i,1), mediaDataTable(i,2), mediaDataTable(i,3), mediaDataTable(i,4)]= computePerformance_cart(prediction, gt, 384);   
-    
+for i= 1:NPred
+    predDir = predDirList(i);
+    evaluePred(predDir);
 end
 
-% output result
-lumenMean = mean(lumenDataTable,1);
-lumenStd = std(lumenDataTable,1);
-mediaMean = mean(mediaDataTable,1);
-mediaStd = std(mediaDataTable,1);
-fprintf("\tJacc\t\tDice\t\tHD\t\tPAD\n")
-fprintf("Lumen:\t")
-for i=1:4
-    fprintf("%.2f%c%.2f\t", lumenMean(i), char(177),lumenStd(i))    
+function result = evaluePred(predDir)
+
+    % predDir = '/raid001/users/hxie1/data/IVUS/polarNumpy/log/SurfaceSubnet_Q/expIVUS_20210514_SurfaceSubnetQ64_100percent_A_skm2/testResult/text';
+    % predDir = '/raid001/users/hxie1/data/IVUS/polarNumpy/log/SurfacesUnet_YufanHe_2/expIVUS_20210514_YufanHe_100percent_A_skm2/testResult/text'
+    % predDir = '/raid001/users/hxie1/data/IVUS/polarNumpy/log/SurfacesUnet/expUnetIVUS_Sigma0_20200302/realtime_testResult/text';
+    % predDir = "/raid001/users/hxie1/data/IVUS/polarNumpy_10percent/log/SurfaceSubnet_Q/expIVUS_20210514_SurfaceSubnetQ64_10percent_A_skm2/testResult/text"
+    # predDir = "/raid001/users/hxie1/data/IVUS/polarNumpy_10percent/log/SurfacesUnet_YufanHe_2/expIVUS_20210514_YufanHe_10percent_A_skm2/testResult/text"
+
+    %gtDir = '/raid001/users/hxie1/data/IVUS/Test_Set/Data_set_B/LABELS_obs1';
+    gtDir = '/raid001/users/hxie1/data/IVUS/Test_Set/Data_set_B/LABELS_obs2_v1'; %Leixin used. It is best.
+    %gtDir = '/raid001/users/hxie1/data/IVUS/Test_Set/Data_set_B/LABELS_obs2_v2';
+    predictLumenFiles = dir(fullfile(predDir,'lum_*_003.txt'));
+    predictLumenFiles = {predictLumenFiles.name}';
+    N = numel(predictLumenFiles);
+    fprintf("==========================================================")
+    fprintf("Prediction result dir: \n%s\n", predDir)
+    fprintf("Test set has %d images comparing with ground truth at %s.\n", N, gtDir)
+
+    % result data tables
+    lumenDataTable = zeros(N, 4);
+    mediaDataTable = zeros(N, 4);
+    % column: jacc, dice, hd, pad
+    % row: different file
+
+    for i=1:N
+        predLumenFile = fullfile(predDir, predictLumenFiles{i});
+        gtLumenFile = fullfile(gtDir, predictLumenFiles{i});
+        prediction = dlmread(predLumenFile, ',')';
+        gt = dlmread(gtLumenFile, ',')';
+        [lumenDataTable(i,1), lumenDataTable(i,2), lumenDataTable(i,3), lumenDataTable(i,4)]= computePerformance_cart(prediction, gt, 384);
+
+        predMediaFile = strrep(predLumenFile,'lum_','med_');
+        gtMediaFile = strrep(gtLumenFile,'lum_','med_');
+        prediction = dlmread(predMediaFile, ',')';
+        gt = dlmread(gtMediaFile, ',')';
+        [mediaDataTable(i,1), mediaDataTable(i,2), mediaDataTable(i,3), mediaDataTable(i,4)]= computePerformance_cart(prediction, gt, 384);
+
+    end
+
+    % output result
+    lumenMean = mean(lumenDataTable,1);
+    lumenStd = std(lumenDataTable,1);
+    mediaMean = mean(mediaDataTable,1);
+    mediaStd = std(mediaDataTable,1);
+    fprintf("\tJacc\t\tDice\t\tHD\t\tPAD\n")
+    fprintf("Lumen:\t")
+    for i=1:4
+        fprintf("%.2f%c%.2f\t", lumenMean(i), char(177),lumenStd(i))
+    end
+    fprintf("\nMedia:\t")
+    for i=1:4
+        fprintf("%.2f%c%.2f\t", mediaMean(i), char(177),mediaStd(i))
+    end
+    fprintf("\n\n")
+    fprintf("==========================================================")
+
 end
-fprintf("\nMedia:\t")
-for i=1:4
-    fprintf("%.2f%c%.2f\t", mediaMean(i), char(177),mediaStd(i))    
-end
-fprintf("\n\n")
+
+
 
