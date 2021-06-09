af3e8d8eaf0edb357d4c9796f5b8e6270f7b207c
Hui Xie <sheenxh@gmail.com>
2021-06-08 21:48:37 -0500

    update

--- OCTSegTool/thinRetina/readMhd_ShowBscan.py
+++ OCTSegTool/thinRetina/readMhd_ShowBscan.py
@@ -29,8 +29,8 @@ def readMhd(mhdPath, indexBscan):
     colSubplot= 1
     f.set_size_inches(W*colSubplot/ float(DPI), H*rowSubplot/ float(DPI))
 
-    #plt.margins(0)
-    # plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)  # very important for erasing unnecessary margins.
+    plt.margins(0)
+    plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)  # very important for erasing unnecessary margins.
 
     plt.imshow(npImage[indexBscan,], cmap='gray')
     plt.axis('off')
