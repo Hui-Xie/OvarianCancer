c2280feda2a9290b135b5196710c7251136a4530
Hui Xie <sheenxh@gmail.com>
2021-06-08 20:36:27 -0500

    update dicom reader

---
+++ OCTSegTool/thinRetina/readCirrusHDOCTDicom_GDCM.py
@@ -0,0 +1,33 @@
+# read Cirrus HD-OCT 6000 dicom, version 11.5.1
+
+DicomDir= "/home/hxie1/data/Ophthalmology/3Dicom/Test/Orig_Output_from_Zeiss/01082671/2017-02-13"
+outputDir = "/home/hxie1/temp/"
+
+import glob as glob
+import pydicom
+from PIL import Image
+from io import BytesIO
+import os
+
+
+dicomFileList = glob.glob(DicomDir + f"/*.dcm")
+dicomFileList.sort()
+
+for dicomPath in dicomFileList:
+    dicomData = pydicom.filereader.dcmread(dicomPath)
+    patientID = dicomData.PatientID
+    visitDate = dicomData.ContentDate
+    ODOS = dicomData.Laterality
+    seriesDescription = dicomData.SeriesDescription.replace(" ", "")
+    outputFilename = f"{patientID}_{visitDate}_{ODOS}_{seriesDescription}.tiff"
+    outputPath = os.path.join(outputDir, outputFilename)
+
+    #pixelData = dicomData.PixelData
+    pixelData = dicomData.pixel_array
+    with Image.open(BytesIO(pixelData)) as im:
+        im.save(outputPath)
+
+
+    print(f"===================")
+    break;
+
---
+++ OCTSegTool/thinRetina/readMhd_ShowBscan.py
@@ -0,0 +1,54 @@
+import sys
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+import os
+
+def printUsage(argv):
+    print("============ Read Nrrd and output one Bscan in current directory =============")
+    print("Usage:")
+    print(argv[0], "  NrrdFilePath   IndexBscan")
+
+
+def readNrrd(nrrdPath, indexBscan):
+    outputDir = os.getcwd()
+    fileName = os.path.splitext(os.path.basename(nrrdPath))[0]
+    fileName = fileName+f"_{indexBscan:03d}.png"
+    outputPath = os.path.join(outputDir, fileName)
+
+    itkImage = sitk.ReadImage(nrrdPath)
+    npImage = sitk.GetArrayFromImage(itkImage)
+
+    S,H,W = npImage.shape
+    print("\nVolume Information:")
+    print(f"Volume name: {nrrdPath}")
+    print(f"Volume shape in #Bscan x PenetrationDepth x #Ascan format: {npImage.shape}")
+
+    f = plt.figure(frameon=False)
+    DPI = 100
+    rowSubplot= 1
+    colSubplot= 1
+    f.set_size_inches(W*colSubplot/ float(DPI), H*rowSubplot/ float(DPI))
+
+    plt.margins(0)
+    plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)  # very important for erasing unnecessary margins.
+
+    plt.imshow(npImage[indexBscan,], cmap='gray')
+    plt.axis('off')
+
+    plt.savefig(outputPath, dpi='figure', bbox_inches='tight', pad_inches=0)
+    plt.close()
+    print("\nOutput:")
+    print(f"output the Bscan {indexBscan} at: {outputPath}\n")
+
+def main():
+    if len(sys.argv) != 3:
+        print("Error: input parameters error.")
+        printUsage(sys.argv)
+        return -1
+
+    readNrrd(sys.argv[1], int(sys.argv[2]))
+
+
+
+if __name__ == "__main__":
+    main()
